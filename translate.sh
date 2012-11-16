f5a8614266d905d4ff8745c8e1a8daf4110ff21d
mcallegari79 <massimocallegari@yahoo.it>
2012-11-16 19:59:16 +0100

    translate script back to work on Ubuntu 12.04

--- translate.sh
+++ translate.sh
@@ -5,12 +5,8 @@ languages="de_DE es_ES fi_FI fr_FR it_IT"
 
 # Compile all files for the given language into one common qlc_<lang>.qm file
 function compile {
-    for file in `find . -name "*_$1.ts"`; do
-        tsfiles="$tsfiles $file"
-    done
-
     echo Processing $1
-    lrelease -silent $tsfiles -qm qlc_$1.qm
+    lrelease -silent `find . -name *_$1.ts` -qm qlc_$1.qm
 }
 
 # Compile all translated languages present in $languages
