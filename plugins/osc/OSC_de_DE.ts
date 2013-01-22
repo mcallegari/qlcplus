4eea6b1b0892b3446559044ab1cbb606a5167c72
mcallegari79 <massimocallegari@yahoo.it>
2013-01-22 23:56:49 +0100

    Updated german translation

--- plugins/osc/OSC_de_DE.ts
+++ plugins/osc/OSC_de_DE.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="configureosc.ui" line="14"/>
         <source>Configure OSC Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>OSC Plugin konfigurieren</translation>
     </message>
     <message>
         <location filename="configureosc.ui" line="20"/>
         <source>Server port number</source>
-        <translation type="unfinished"></translation>
+        <translation>Server-Portnummer</translation>
     </message>
 </context>
 <context>
@@ -19,7 +19,7 @@
     <message>
         <location filename="oscplugin.cpp" line="69"/>
         <source>This plugin provides input for devices supporting the OSC transmission protocol.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dieses Plugin bietet Eingabunterstützung für Geräte mit dem OSC-Protokoll.</translation>
     </message>
 </context>
 </TS>
--- plugins/velleman/src/Velleman_de_DE.ts
+++ plugins/velleman/src/Velleman_de_DE.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="velleman.cpp" line="123"/>
         <source>This plugin provides DMX output support for the Velleman K8062D using the DLL supplied with the product.</source>
-        <translation type="unfinished">Dieses Plugin bietet DMX-Output für den Velleman K8062D und verwendet die vom Produkt mitgelieferte DLL.</translation>
+        <translation>Dieses Plugin bietet DMX-Output für den Velleman K8062D und verwendet die vom Produkt mitgelieferte DLL.</translation>
     </message>
 </context>
 </TS>
