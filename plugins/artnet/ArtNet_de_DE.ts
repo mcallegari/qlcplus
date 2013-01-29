24b13be2f7db8d7942e997d13f615fb19a2b8a20
mcallegari79 <massimocallegari@yahoo.it>
2013-01-29 11:09:33 +0100

    Include ArtNet plugin into debian package
    ArtNet plugin german translation

--- plugins/artnet/ArtNet_de_DE.ts
+++ plugins/artnet/ArtNet_de_DE.ts
@@ -7,13 +7,13 @@
         <location filename="artnetplugin.cpp" line="102"/>
         <source>This plugin provides DMX output for devices supporting the ArtNet communication protocol.</source>
         <oldsource>This plugin provides output for devices supporting the ArtNet communication protocol.</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Dieses Plugin bietet Unterstützung für Geräte mit dem ArtNet-Kommunikationsprotokoll.</translation>
     </message>
     <message>
         <location filename="artnetplugin.cpp" line="116"/>
         <source>%1: [%2] Output: %3</source>
         <oldsource>%1: [%2] Address: %3</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>%1: [%2] Ausgabe: %3</translation>
     </message>
 </context>
 <context>
@@ -21,47 +21,48 @@
     <message>
         <location filename="configureartnet.ui" line="6"/>
         <source>Configure ArtNet Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>ArtNet-Plugin konfigurieren</translation>
     </message>
     <message>
         <location filename="configureartnet.ui" line="16"/>
         <source>Output Mapping</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment> </translatorcomment>
+        <translation>Ausgabe Zuweisung (Mapping)</translation>
     </message>
     <message>
         <location filename="configureartnet.ui" line="29"/>
         <source>Network</source>
-        <translation type="unfinished"></translation>
+        <translation>Netzwerk</translation>
     </message>
     <message>
         <location filename="configureartnet.ui" line="34"/>
         <source>Address</source>
-        <translation type="unfinished"></translation>
+        <translation>Adresse</translation>
     </message>
     <message>
         <location filename="configureartnet.ui" line="43"/>
         <source>Nodes Tree</source>
-        <translation type="unfinished"></translation>
+        <translation>Knoten (Nodes) Baum</translation>
     </message>
     <message>
         <location filename="configureartnet.ui" line="53"/>
         <source>IP</source>
-        <translation type="unfinished"></translation>
+        <translation>IP</translation>
     </message>
     <message>
         <location filename="configureartnet.ui" line="58"/>
         <source>Short Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Abgekürzter Name</translation>
     </message>
     <message>
         <location filename="configureartnet.ui" line="63"/>
         <source>Long Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Kompletter Name</translation>
     </message>
     <message>
         <location filename="configureartnet.cpp" line="82"/>
         <source>Universe %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Universum %1</translation>
     </message>
 </context>
 </TS>
