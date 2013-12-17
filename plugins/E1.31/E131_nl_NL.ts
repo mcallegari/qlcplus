920a668d4ce19e5c2affdfbb4c42e5d78eb9c2e9
joepadmiraal <joep@groovytunes.nl>
2013-12-17 21:56:56 +0100

    Updated Dutch translations 4.6.0

--- plugins/E1.31/E131_nl_NL.ts
+++ plugins/E1.31/E131_nl_NL.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="configuree131.ui" line="14"/>
         <source>Configure E1.31 Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Configureer E1 31 Plugin</translation>
     </message>
     <message>
         <location filename="configuree131.ui" line="24"/>
         <source>Network Mapping</source>
-        <translation type="unfinished"></translation>
+        <translation>Netwerk Mapping</translation>
     </message>
     <message>
         <location filename="configuree131.ui" line="37"/>
         <source>Network</source>
-        <translation type="unfinished"></translation>
+        <translation>Netwerk</translation>
     </message>
     <message>
         <location filename="configuree131.ui" line="42"/>
         <source>Universe</source>
-        <translation type="unfinished"></translation>
+        <translation>Universe</translation>
     </message>
     <message>
         <location filename="configuree131.cpp" line="76"/>
         <source>Universe %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Universe %1</translation>
     </message>
 </context>
 <context>
@@ -34,45 +34,45 @@
     <message>
         <location filename="e131plugin.cpp" line="107"/>
         <source>This plugin provides DMX output for devices supporting the E1.31 communication protocol.</source>
-        <translation type="unfinished"></translation>
+        <translation>Deze plugin verzorgt DMX output voor apparaten die het E1 31 communicatie protocol ondersteunen.</translation>
     </message>
     <message>
         <location filename="e131plugin.cpp" line="123"/>
         <location filename="e131plugin.cpp" line="234"/>
         <source>%1: [%2] Universe: %3</source>
-        <translation type="unfinished"></translation>
+        <translation>%1: [%2] Universe: %3</translation>
     </message>
     <message>
         <location filename="e131plugin.cpp" line="136"/>
         <source>Output</source>
-        <translation type="unfinished"></translation>
+        <translation>Output</translation>
     </message>
     <message>
         <location filename="e131plugin.cpp" line="140"/>
         <location filename="e131plugin.cpp" line="316"/>
         <source>Status: Not open</source>
-        <translation type="unfinished"></translation>
+        <translation>Status: Niet open</translation>
     </message>
     <message>
         <location filename="e131plugin.cpp" line="143"/>
         <location filename="e131plugin.cpp" line="319"/>
         <source>Status: Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Status: Open</translation>
     </message>
     <message>
         <location filename="e131plugin.cpp" line="145"/>
         <source>Packets sent: </source>
-        <translation type="unfinished"></translation>
+        <translation>Pakketten verzonden:</translation>
     </message>
     <message>
         <location filename="e131plugin.cpp" line="312"/>
         <source>Input</source>
-        <translation type="unfinished"></translation>
+        <translation>Input</translation>
     </message>
     <message>
         <location filename="e131plugin.cpp" line="321"/>
         <source>Packets received: </source>
-        <translation type="unfinished"></translation>
+        <translation>Pakketten ontvangen:</translation>
     </message>
 </context>
 </TS>
