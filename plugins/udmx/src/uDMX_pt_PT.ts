f251dd1d62f40dba88821f19462f5174092b38da
interzona <copymerge@gmail.com>
2014-03-11 13:15:42 +0000

    I think this is it! Pls let me know if it is everything ok

---
+++ plugins/udmx/src/uDMX_pt_PT.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_PT">
+<context>
+    <name>UDMX</name>
+    <message>
+        <location filename="udmx.cpp" line="94"/>
+        <source>This plugin provides DMX output support for Anyma uDMX devices.</source>
+        <translation>Este plugin fornece suporte de saída DMX para dispositivos Anyma uDMX.</translation>
+    </message>
+    <message>
+        <location filename="udmx.cpp" line="188"/>
+        <source>Do you wish to re-scan your hardware?</source>
+        <translation>Deseja voltar a examinar o seu hardware?</translation>
+    </message>
+</context>
+<context>
+    <name>UDMXDevice</name>
+    <message>
+        <location filename="udmxdevice.cpp" line="114"/>
+        <source>Unknown</source>
+        <translation>Desconhecido</translation>
+    </message>
+    <message>
+        <location filename="udmxdevice.cpp" line="135"/>
+        <source>DMX Frame Frequency</source>
+        <translation>Frequência de Frames DMX</translation>
+    </message>
+    <message>
+        <location filename="udmxdevice.cpp" line="138"/>
+        <source>Bad</source>
+        <translation>Má</translation>
+    </message>
+    <message>
+        <location filename="udmxdevice.cpp" line="140"/>
+        <source>Good</source>
+        <translation>Boa</translation>
+    </message>
+    <message>
+        <location filename="udmxdevice.cpp" line="142"/>
+        <source>Patch this device to a universe to find out.</source>
+        <translation>Efectuar patch deste dispositivo a um universo para descobrir.</translation>
+    </message>
+    <message>
+        <location filename="udmxdevice.cpp" line="143"/>
+        <source>System Timer Accuracy</source>
+        <translation>Precisão do relógio de sistema</translation>
+    </message>
+    <message>
+        <location filename="udmxdevice.cpp" line="148"/>
+        <source>Unknown device</source>
+        <translation>Dispositivo desconhecido</translation>
+    </message>
+    <message>
+        <location filename="udmxdevice.cpp" line="150"/>
+        <source>Cannot connect to USB device.</source>
+        <translation>Não se consegue ligar a um dispositivo USB.</translation>
+    </message>
+</context>
+</TS>
---
+++ plugins/velleman/src/Velleman_pt_PT.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_PT">
+<context>
+    <name>Velleman</name>
+    <message>
+        <location filename="velleman.cpp" line="121"/>
+        <source>This plugin provides DMX output support for the Velleman K8062D using the DLL supplied with the product.</source>
+        <translation>Este plugin fornece suporte de saída DMX para o interface Velleman K8062D usando a DLL fornecida com o produto.</translation>
+    </message>
+</context>
+</TS>
