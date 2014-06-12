3d1b69da25bd6c6c73d985f5a7e62f096fcccc3a
Kokichiro <ahoaho0992@yahoo.co.jp>
2014-06-13 00:07:20 +0900

    Update launcher_ja_JP.ts

--- launcher/launcher_ja_JP.ts
+++ launcher/launcher_ja_JP.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="launcher.cpp" line="34"/>
         <source>Launch %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">%1 を開始</translation>
     </message>
     <message>
         <location filename="launcher.cpp" line="39"/>
         <source>Launch the main %1 application</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">アプリケーション %1 を開始</translation>
     </message>
 </context>
 </TS>
