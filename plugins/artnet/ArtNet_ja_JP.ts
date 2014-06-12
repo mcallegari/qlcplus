e66fc895c00eb48c46d6d87c22cc10bebb1b7d40
Kokichiro <ahoaho0992@yahoo.co.jp>
2014-06-12 23:57:33 +0900

    Update ArtNet_ja_JP.ts

--- plugins/artnet/ArtNet_ja_JP.ts
+++ plugins/artnet/ArtNet_ja_JP.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="artnetplugin.cpp" line="73"/>
         <source>This plugin provides DMX output for devices supporting the ArtNet communication protocol.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">このプラグインは、ArtNet対応デバイスとの間でDMX信号を送受信します。</translation>
     </message>
     <message>
         <location filename="artnetplugin.cpp" line="90"/>
@@ -17,39 +17,39 @@
     <message>
         <location filename="artnetplugin.cpp" line="106"/>
         <source>Output</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">出力</translation>
     </message>
     <message>
         <location filename="artnetplugin.cpp" line="110"/>
         <location filename="artnetplugin.cpp" line="266"/>
         <source>Status: Not open</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">状態: 未接続</translation>
     </message>
     <message>
         <location filename="artnetplugin.cpp" line="113"/>
         <location filename="artnetplugin.cpp" line="269"/>
         <source>Status: Open</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">状態: 接続</translation>
     </message>
     <message>
         <location filename="artnetplugin.cpp" line="115"/>
         <source>Nodes discovered: </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">見つかったノード:</translation>
     </message>
     <message>
         <location filename="artnetplugin.cpp" line="118"/>
         <source>Packets sent: </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">送信されたパケット:</translation>
     </message>
     <message>
         <location filename="artnetplugin.cpp" line="262"/>
         <source>Input</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">入力</translation>
     </message>
     <message>
         <location filename="artnetplugin.cpp" line="271"/>
         <source>Packets received: </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">受信したパケット:</translation>
     </message>
 </context>
 <context>
@@ -57,27 +57,27 @@
     <message>
         <location filename="configureartnet.ui" line="14"/>
         <source>Configure ArtNet Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ArtNetプラグイン設定</translation>
     </message>
     <message>
         <location filename="configureartnet.ui" line="24"/>
         <source>Nodes Tree</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Nodes Tree</translation>
     </message>
     <message>
         <location filename="configureartnet.ui" line="34"/>
         <source>IP</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">IP</translation>
     </message>
     <message>
         <location filename="configureartnet.ui" line="39"/>
         <source>Short Name</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Short Name</translation>
     </message>
     <message>
         <location filename="configureartnet.ui" line="44"/>
         <source>Long Name</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Long Name</translation>
     </message>
 </context>
 </TS>
