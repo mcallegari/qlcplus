4c9d965a5b65688392bbd42ce52543f8b6e77c28
Kokichiro <ahoaho0992@yahoo.co.jp>
2014-06-13 00:05:14 +0900

    Update uDMX_ja_JP.ts

--- plugins/udmx/src/uDMX_ja_JP.ts
+++ plugins/udmx/src/uDMX_ja_JP.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="udmx.cpp" line="94"/>
         <source>This plugin provides DMX output support for Anyma uDMX devices.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">このプラグインは、uDMX(μDMX)デバイスにDMX信号を送信します。</translation>
     </message>
     <message>
         <location filename="udmx.cpp" line="188"/>
         <source>Do you wish to re-scan your hardware?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ハードウェアを再スキャンしますか？</translation>
     </message>
 </context>
 <context>
@@ -19,42 +19,42 @@
     <message>
         <location filename="udmxdevice.cpp" line="114"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Unknown</translation>
     </message>
     <message>
         <location filename="udmxdevice.cpp" line="135"/>
         <source>DMX Frame Frequency</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">DMX Frame Frequency</translation>
     </message>
     <message>
         <location filename="udmxdevice.cpp" line="138"/>
         <source>Bad</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Bad</translation>
     </message>
     <message>
         <location filename="udmxdevice.cpp" line="140"/>
         <source>Good</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Good</translation>
     </message>
     <message>
         <location filename="udmxdevice.cpp" line="142"/>
         <source>Patch this device to a universe to find out.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Patch this device to a universe to find out.</translation>
     </message>
     <message>
         <location filename="udmxdevice.cpp" line="143"/>
         <source>System Timer Accuracy</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">System Timer Accuracy</translation>
     </message>
     <message>
         <location filename="udmxdevice.cpp" line="148"/>
         <source>Unknown device</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Unknown device</translation>
     </message>
     <message>
         <location filename="udmxdevice.cpp" line="150"/>
         <source>Cannot connect to USB device.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Cannot connect to USB device.</translation>
     </message>
 </context>
 </TS>
