95bc303b8624e1a2371f9dadbfe8aa0b59da218a
plugz <david.garyga@gmail.com>
2014-11-17 12:11:26 +0100

    Fix issue with function staying ON in a fast running chaser

--- engine/src/chaserrunner.cpp
+++ engine/src/chaserrunner.cpp
@@ -404,7 +404,7 @@ void ChaserRunner::clearRunningList()
     // empty the running queue
     foreach(ChaserRunnerStep *step, m_runnerSteps)
     {
-        if (step->m_function != NULL && step->m_function->isRunning())
+        if (step->m_function != NULL && !step->m_function->stopped())
         {
             step->m_function->stop();
             step->m_function = NULL;
@@ -601,7 +601,7 @@ bool ChaserRunner::write(MasterTimer* timer, QList<Universe *> universes)
         if (step->m_duration != Function::infiniteSpeed() &&
              step->m_elapsed >= step->m_duration)
         {
-            if (step->m_function != NULL && step->m_function->isRunning())
+            if (step->m_function != NULL && !step->m_function->stopped())
             {
                 step->m_function->stop();
                 step->m_function = NULL;
