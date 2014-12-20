95153eb54aee3892a56be8d18c0ac572cd58ec4c
Massimo Callegari <massimocallegari@yahoo.it>
2014-12-20 16:20:56 +0100

    UI: fixed time editing of milliseconds (.3 is 300ms and not 30ms)

--- engine/src/function.cpp
+++ engine/src/function.cpp
@@ -558,7 +558,14 @@ uint Function::stringToSpeed(QString speed)
 
     QStringList msecs = speed.split(".");
     if (msecs.count() > 0)
-        value += (msecs.at(msecs.count() - 1).toUInt() * 10);
+    {
+        QString msecStr = msecs.at(msecs.count() - 1);
+        uint msecInt = msecStr.toUInt();
+        if (msecInt < 10 && msecStr.contains("0") == false)
+            value += (msecInt * 100);
+        else
+            value += (msecInt * 10);
+    }
 
     return value;
 }
