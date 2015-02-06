adeafb9cf8d36d37046ec2537cf35f33c1239b3d
Massimo Callegari <massimocallegari@yahoo.it>
2015-02-06 19:57:05 +0100

    Revert loader/main.cpp change not building on OSX
    Caused by 12ec72518abadfc0cefecfd685ad4a635369a895

--- launcher/main.cpp
+++ launcher/main.cpp
@@ -11,7 +11,12 @@
 void loadTranslation(const QString& locale, QApplication& app)
 {
     QString file(QString("launcher_%1").arg(locale));
-    QString path = QLCFile::systemDirectory(TRANSLATIONDIR).path();
+#if defined(__APPLE__) || defined(Q_OS_MAC)
+    QString path(QString("%1/../%2").arg(QApplication::applicationDirPath())
+                 .arg(TRANSLATIONDIR));
+#else
+    QString path(TRANSLATIONDIR);
+#endif
 
     QTranslator* translator = new QTranslator(&app);
     if (translator->load(file, path) == true)
