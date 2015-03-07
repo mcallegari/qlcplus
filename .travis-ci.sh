1731bb8619f6abe18f15e0436823891a9ed3694e
Peter Newman <peternewman@users.noreply.github.com>
2015-03-07 13:19:02 +0000

    Create .travis-ci.sh

---
+++ .travis-ci.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script is triggered from the script section of .travis.yml
+# It runs the appropriate commands depending on the task requested.
+
+# Otherwise compile and check as normal
+qmake && make
