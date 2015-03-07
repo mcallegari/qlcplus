ef0dcf3edeed32694ab8b772f8ac9a8cf4260fa8
Peter Newman <peternewman@users.noreply.github.com>
2015-03-07 15:06:01 +0000

    Try and fix the X server error

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,4 +4,4 @@
 # It runs the appropriate commands depending on the task requested.
 
 # Otherwise compile and check as normal
-qmake && make && ./unittest.sh
+qmake && make && xvfb-run --server-args="-screen 0, 1024x768x24" ./unittest.sh
