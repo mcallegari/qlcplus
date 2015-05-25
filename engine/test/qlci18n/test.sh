cb3af27795e93f4789ccf64c2f8ea81e3d468b73
Jano Svitok <jan.svitok@gmail.com>
2015-05-25 10:11:30 +0200

    Unit test: ignore changes to .ts file

--- engine/test/qlci18n/test.sh
+++ engine/test/qlci18n/test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=../../src
 export DYLD_FALLBACK_LIBRARY_PATH=../../src
+cp qlci18n_fi_FI.ts_ qlci18n_fi_FI.ts
 if [ -n "$QTDIR" ]; then
     $QTDIR/bin/lupdate qlci18n.pro
     $QTDIR/bin/lrelease qlci18n.pro
