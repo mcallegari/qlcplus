2aca82ae1a7ef09c5a93bb5b6cf5b9a569994b86
Jano Svitok <jan.svitok@gmail.com>
2015-06-11 23:08:13 +0200

    Simple fix for "Apply changes to fixtures of same type" & dimmers. Fixes #457.

--- ui/src/channelsselection.h
+++ ui/src/channelsselection.h
@@ -77,6 +77,9 @@ protected:
     void updateFixturesTree();
     QList<QTreeWidgetItem *> getSameChannels(QTreeWidgetItem *item);
 
+private:
+    QList<QTreeWidgetItem *> getDimmerChannels(QTreeWidgetItem *item);
+
 protected slots:
 
     void slotItemChecked(QTreeWidgetItem *item, int col);
