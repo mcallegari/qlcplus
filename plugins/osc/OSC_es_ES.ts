5d9f8ce96cef1ea1de633ab0733b7405a62a3df7
Luis García-Tornel <tornel@gmail.com>
2015-09-23 14:12:47 -0400

    Update spanish translation (UI and OSC)

--- plugins/osc/OSC_es_ES.ts
+++ plugins/osc/OSC_es_ES.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="es_ES">
+<TS version="2.1" language="es_ES">
 <context>
     <name>ConfigureOSC</name>
     <message>
@@ -52,13 +52,14 @@
     <message>
         <location filename="configureosc.cpp" line="168"/>
         <source>Invalid IP</source>
-        <translation type="unfinished"></translation>
+        <translation>IP Inválido</translation>
     </message>
     <message>
         <location filename="configureosc.cpp" line="168"/>
         <source>%1 is not a valid IP.
 Please fix it before confirming.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 no es un IP válidoo.
+Por favor arréglelo antes de confirmar.</translation>
     </message>
 </context>
 <context>
