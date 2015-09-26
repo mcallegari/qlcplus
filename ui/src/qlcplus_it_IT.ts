9c9cc1eee923a519495231adb532277696e4aa3f
Luis García-Tornel <tornel@gmail.com>
2015-09-26 10:35:31 -0400

    Update spanish translation (UI and E1.31)

--- ui/src/qlcplus_it_IT.ts
+++ ui/src/qlcplus_it_IT.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="it_IT">
+<TS version="2.1" language="it_IT">
 <context>
     <name>AboutBox</name>
     <message>
@@ -2012,7 +2012,7 @@ Il file selezionato è stato spostato o eliminato.</translation>
         <source>Rotation of the pattern&apos;s starting point</source>
         <translation>Punto di partenza della rotazione del modello</translation>
     </message>
-    <message utf8="true">
+    <message>
         <location filename="efxeditor.ui" line="312"/>
         <location filename="efxeditor.ui" line="410"/>
         <source>°</source>
