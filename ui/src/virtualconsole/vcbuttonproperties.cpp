a54e447af26ddb0479830d4a579abf36a5bd7210
Massimo Callegari <massimocallegari@yahoo.it>
2015-09-29 20:09:10 +0200

    ui: created a dedicated widget to set external inputs
    Applied to every VC widget properties page, this should clean up the code a lot
    
    + updated changelog and FixturesMap

--- ui/src/virtualconsole/vcbuttonproperties.cpp
+++ ui/src/virtualconsole/vcbuttonproperties.cpp
@@ -32,19 +32,18 @@
 #include "qlcinputprofile.h"
 #include "qlcfixturedef.h"
 
+#include "inputselectionwidget.h"
 #include "vcbuttonproperties.h"
-#include "selectinputchannel.h"
 #include "functionselection.h"
 #include "speeddialwidget.h"
 #include "virtualconsole.h"
-#include "assignhotkey.h"
-#include "inputpatch.h"
 #include "function.h"
 #include "fixture.h"
 #include "doc.h"
 
 VCButtonProperties::VCButtonProperties(VCButton* button, Doc* doc)
     : QDialog(button)
+    , m_button(button)
     , m_doc(doc)
     , m_speedDials(NULL)
 {
@@ -53,23 +52,21 @@ VCButtonProperties::VCButtonProperties(VCButton* button, Doc* doc)
 
     setupUi(this);
 
+    m_inputSelWidget = new InputSelectionWidget(m_doc, this);
+    m_inputSelWidget->setKeySequence(m_button->keySequence());
+    m_inputSelWidget->setInputSource(m_button->inputSource());
+    m_inputSelWidget->setWidgetPage(m_button->page());
+    m_inputSelWidget->show();
+    m_extControlLayout->addWidget(m_inputSelWidget);
+
     QAction* action = new QAction(this);
     action->setShortcut(QKeySequence(QKeySequence::Close));
     connect(action, SIGNAL(triggered(bool)), this, SLOT(reject()));
     addAction(action);
 
     /* Button text and function */
-    m_button = button;
     m_nameEdit->setText(m_button->caption());
     slotSetFunction(button->function());
-
-    /* Key sequence */
-    m_keySequence = QKeySequence(button->keySequence());
-    m_keyEdit->setText(m_keySequence.toString(QKeySequence::NativeText));
-
-    /* External input */
-    m_inputSource = m_button->inputSource();
-    updateInputSource();
 
     /* Press action */
     if (button->action() == VCButton::Flash)
@@ -95,9 +92,6 @@ VCButtonProperties::VCButtonProperties(VCButton* button, Doc* doc)
     connect(m_attachFunction, SIGNAL(clicked()), this, SLOT(slotAttachFunction()));
     connect(m_detachFunction, SIGNAL(clicked()), this, SLOT(slotSetFunction()));
 
-    connect(m_attachKey, SIGNAL(clicked()), this, SLOT(slotAttachKey()));
-    connect(m_detachKey, SIGNAL(clicked()), this, SLOT(slotDetachKey()));
-
     connect(m_toggle, SIGNAL(toggled(bool)), this, SLOT(slotActionToggled()));
     connect(m_blackout, SIGNAL(toggled(bool)), this, SLOT(slotActionToggled()));
     connect(m_stopAll, SIGNAL(toggled(bool)), this, SLOT(slotActionToggled()));
@@ -105,11 +99,6 @@ VCButtonProperties::VCButtonProperties(VCButton* button, Doc* doc)
 
     connect(m_speedDialButton, SIGNAL(toggled(bool)),
             this, SLOT(slotSpeedDialToggle(bool)));
-
-    connect(m_autoDetectInputButton, SIGNAL(toggled(bool)),
-            this, SLOT(slotAutoDetectInputToggled(bool)));
-    connect(m_chooseInputButton, SIGNAL(clicked()),
-            this, SLOT(slotChooseInputClicked()));
 
     connect(m_intensitySlider, SIGNAL(valueChanged(int)),
             this, SLOT(slotIntensitySliderMoved(int)));
@@ -154,69 +143,6 @@ void VCButtonProperties::slotSetFunction(quint32 fid)
             m_attributesList->addItem(item);
         }
     }
-}
-
-void VCButtonProperties::slotAttachKey()
-{
-    AssignHotKey ahk(this, m_keySequence);
-    if (ahk.exec() == QDialog::Accepted)
-    {
-        m_keySequence = QKeySequence(ahk.keySequence());
-        m_keyEdit->setText(m_keySequence.toString(QKeySequence::NativeText));
-    }
-}
-
-void VCButtonProperties::slotDetachKey()
-{
-    m_keySequence = QKeySequence();
-    m_keyEdit->setText(m_keySequence.toString(QKeySequence::NativeText));
-}
-
-void VCButtonProperties::slotAutoDetectInputToggled(bool checked)
-{
-    if (checked == true)
-    {
-        connect(m_doc->inputOutputMap(),
-                SIGNAL(inputValueChanged(quint32,quint32,uchar)),
-                this, SLOT(slotInputValueChanged(quint32,quint32)));
-    }
-    else
-    {
-        disconnect(m_doc->inputOutputMap(),
-                   SIGNAL(inputValueChanged(quint32,quint32,uchar)),
-                   this, SLOT(slotInputValueChanged(quint32,quint32)));
-    }
-}
-
-void VCButtonProperties::slotInputValueChanged(quint32 universe, quint32 channel)
-{
-    m_inputSource = QSharedPointer<QLCInputSource>(new QLCInputSource(universe, (m_button->page() << 16) | channel));
-    updateInputSource();
-}
-
-void VCButtonProperties::slotChooseInputClicked()
-{
-    SelectInputChannel sic(this, m_doc->inputOutputMap());
-    if (sic.exec() == QDialog::Accepted)
-    {
-        m_inputSource = QSharedPointer<QLCInputSource>(new QLCInputSource(sic.universe(), sic.channel()));
-        updateInputSource();
-    }
-}
-
-void VCButtonProperties::updateInputSource()
-{
-    QString uniName;
-    QString chName;
-
-    if (m_doc->inputOutputMap()->inputSourceNames(m_inputSource, uniName, chName) == false)
-    {
-        uniName = KInputNone;
-        chName = KInputNone;
-    }
-
-    m_inputUniverseEdit->setText(uniName);
-    m_inputChannelEdit->setText(chName);
 }
 
 void VCButtonProperties::slotActionToggled()
@@ -293,8 +219,8 @@ void VCButtonProperties::accept()
 {
     m_button->setCaption(m_nameEdit->text());
     m_button->setFunction(m_function);
-    m_button->setKeySequence(m_keySequence);
-    m_button->setInputSource(m_inputSource);
+    m_button->setKeySequence(m_inputSelWidget->keySequence());
+    m_button->setInputSource(m_inputSelWidget->inputSource());
     m_button->enableStartupIntensity(m_intensityGroup->isChecked());
     m_button->setStartupIntensity(qreal(m_intensitySlider->value()) / qreal(100));
 
