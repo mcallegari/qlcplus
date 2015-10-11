539b013511a23b27a7a010780db14a247232d98a
Jannis Achstetter <kripton@kripserver.net>
2015-10-11 12:09:25 +0200

    Add previously non-existant .ts-files (Mainly GPIO-, Dummy- and UART-plugin)

---
+++ plugins/dummy/Dummy_ja_JP.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
+<context>
+    <name>DummyConfiguration</name>
+    <message>
+        <location filename="dummyconfiguration.ui" line="14"/>
+        <source>Configure Dummy Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummyconfiguration.ui" line="27"/>
+        <source>Remove me and add the necessary controls to configure the plugin specific settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DummyPlugin</name>
+    <message>
+        <location filename="dummyplugin.cpp" line="106"/>
+        <source>This plugin provides dummy input/output for dummy devices.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ plugins/gpio/GPIO_ja_JP.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
+<context>
+    <name>GPIOConfiguration</name>
+    <message>
+        <location filename="gpioconfiguration.ui" line="14"/>
+        <source>Configure GPIO Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gpioconfiguration.ui" line="28"/>
+        <source>GPIO Number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gpioconfiguration.ui" line="33"/>
+        <source>Usage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gpioconfiguration.cpp" line="57"/>
+        <source>Not used</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gpioconfiguration.cpp" line="58"/>
+        <source>Input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gpioconfiguration.cpp" line="59"/>
+        <source>Output</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GPIOPlugin</name>
+    <message>
+        <location filename="gpioplugin.cpp" line="84"/>
+        <source>This plugin provides input/output on GPIO PINs.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
