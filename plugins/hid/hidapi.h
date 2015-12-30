caf52b18e9f28d3ed442b137fc9d712d01e1d53a
Dave Olsthoorn <dave.olsthoorn@gmail.com>
2015-12-31 00:08:10 +0100

    Fix license in hidapi
    Fix license in plugins/hid/hidapi.h to match plugins/hid/macx/hidapi.cpp
    etc.

--- plugins/hid/hidapi.h
+++ plugins/hid/hidapi.h
@@ -1,24 +1,25 @@
-/*******************************************************
- HIDAPI - Multi-Platform library for
- communication with HID devices.
-
- Alan Ott
- Signal 11 Software
-
- 8/22/2009
-
- Copyright 2009, All Rights Reserved.
-
- At the discretion of the user of this library,
- this software may be licensed under the terms of the
- GNU General Public License v3, a BSD-Style license, or the
- original HIDAPI license as outlined in the LICENSE.txt,
- LICENSE-gpl3.txt, LICENSE-bsd.txt, and LICENSE-orig.txt
- files located at the root of the source distribution.
- These files may also be found in the public source
- code repository located at:
-        http://github.com/signal11/hidapi .
-********************************************************/
+/*
+ * HIDAPI - Multi-Platform library for
+ * communication with HID devices.
+ *
+ * Copyright (c) 2010 Alan Ott - Signal 11 Software
+ *
+ * 2010-07-03
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 /** @file
  * @defgroup API hidapi API
