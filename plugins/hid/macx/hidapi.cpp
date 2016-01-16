5649d6cd42dbdd89b2dc7f4b5f1d1361fd8d469a
Massimo Callegari <massimocallegari@yahoo.it>
2016-01-16 14:05:38 +0100

    plugin/HID: again, use the original, more flexible license

--- plugins/hid/macx/hidapi.cpp
+++ plugins/hid/macx/hidapi.cpp
@@ -6,18 +6,9 @@
 
   2010-07-03
 
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  This software may be used by anyone for any reason so
+  long as the copyright notice in the source files
+  remains intact.
 */
 
 /* See Apple Technical Note TN2187 for details on IOHidManager. */
