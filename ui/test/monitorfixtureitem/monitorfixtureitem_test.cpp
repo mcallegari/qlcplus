89fa4dd57f4a0d4130cf2b3dc3caca3d14177f30
Massimo Callegari <massimocallegari@yahoo.it>
2016-11-20 12:20:05 +0100

    Updated changelog and credits

--- ui/test/monitorfixtureitem/monitorfixtureitem_test.cpp
+++ ui/test/monitorfixtureitem/monitorfixtureitem_test.cpp
@@ -2,7 +2,7 @@
   Q Light Controller Plus
   monitorfixtureitem_test.cpp
 
-  Copyright (C) Heikki Junnila
+  Copyright (C) Jano Svitok
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
