f4c755ddf4e4c4aa1acace5d93d620b5a18975c8
Massimo Callegari <massimocallegari@yahoo.it>
2017-04-02 21:28:41 +0200

    ui/vcframe: restore caption when disabling multipage

--- ui/src/virtualconsole/vcframe.cpp
+++ ui/src/virtualconsole/vcframe.cpp
@@ -442,6 +442,7 @@ void VCFrame::setMultipageMode(bool enable)
         m_prevPageBtn = NULL;
         m_pageCombo = NULL;
         m_nextPageBtn = NULL;
+        setCaption(caption());
     }
 
     m_multiPageMode = enable;
