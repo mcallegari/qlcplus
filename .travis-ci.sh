f95fcf80fbef372034bd806326a987e43d71d991
Massimo Callegari <massimocallegari@yahoo.it>
2017-07-09 13:27:34 +0200

    travis: exit with the proper result

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -6,7 +6,9 @@
 # Otherwise compile and check as normal
 if [ "$TASK" = "compile" ]; then
 $QMAKE QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX && make && ./unittest.sh
+exit $?
 fi
 if [ "$TASK" = "coverage" ]; then
 $QMAKE CONFIG+=coverage QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX && make && ./coverage.sh
+exit $?
 fi
