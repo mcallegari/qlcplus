a68cb974006c618a175059b8232bed18eaf6227e
Massimo Callegari <massimocallegari@yahoo.it>
2017-07-09 15:57:54 +0200

    Revert "travis: try passing variables to qmake as strings"
    This reverts commit c7168ed496f0cf76f6833a058a4b1652a93ed3a5.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -5,10 +5,10 @@
 
 # Otherwise compile and check as normal
 if [ "$TASK" = "compile" ]; then
-$QMAKE QMAKE_CXX="$CXX" QMAKE_CC="$CC" QMAKE_LINK="$CXX" QMAKE_LINK_SHLIB="$CXX" && make && ./unittest.sh
+$QMAKE QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX && make && ./unittest.sh
 exit $?
 fi
 if [ "$TASK" = "coverage" ]; then
-$QMAKE CONFIG+=coverage QMAKE_CXX="$CXX" QMAKE_CC="$CC" QMAKE_LINK="$CXX" QMAKE_LINK_SHLIB="$CXX" && make && ./coverage.sh
+$QMAKE CONFIG+=coverage QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX && make && ./coverage.sh
 exit $?
 fi
