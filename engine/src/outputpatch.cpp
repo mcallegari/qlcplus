mcallegari/qlcplus

commit 7ad078d13ce48367d6b263f85e1783bfc6fd31af

engine: keep track of input/output device lines even if they are disconnected (fix #994)

engine/src/outputpatch.cpp
@@ -133,10 +133,7 @@ QString OutputPatch::outputName() const
 
 quint32 OutputPatch::output() const
 {
-    if (m_plugin != NULL && m_pluginLine < quint32(m_plugin->outputs().size()))
-        return m_pluginLine;
-    else
-        return QLCIOPlugin::invalidLine();
+    return m_pluginLine;
 }
 
 bool OutputPatch::isPatched() const

engine/src/inputpatch.cpp
@@ -75,6 +75,10 @@ bool InputPatch::set(QLCIOPlugin* plugin, quint32 input, QLCInputProfile* profil
 {
     bool result = false;
 
+    qDebug() << "InputPatch::set - plugin:" << ((plugin == NULL)?"None":plugin->name())
+             << ", line:" << input
+             << ", profile:" << ((profile == NULL)?"None":profile->name());
+
     if (m_plugin != NULL && m_pluginLine != QLCIOPlugin::invalidLine())
     {
         disconnect(m_plugin, SIGNAL(valueChanged(quint32,quint32,quint32,uchar,QString)),
@@ -162,10 +166,7 @@ QString InputPatch::pluginName() const
 
 quint32 InputPatch::input() const
 {
-    if (m_plugin != NULL && m_pluginLine < quint32(m_plugin->inputs().count()))
-        return m_pluginLine;
-    else
-        return QLCIOPlugin::invalidLine();
+    return m_pluginLine;
 }
 
 QString InputPatch::inputName() const

commit c19f792a6229e070d98145ee16a0ed90334def82

engine: fix crash caused by NULL plugin
Most likely this is a regression caused by 7ad078d13ce48367d6b263f85e1783bfc6fd31af

engine/src/outputpatch.cpp
@@ -138,7 +138,7 @@ quint32 OutputPatch::output() const
 
 bool OutputPatch::isPatched() const
 {
-    return output() != QLCIOPlugin::invalidLine();
+    return output() != QLCIOPlugin::invalidLine() && m_plugin != NULL;
 }
 
 void OutputPatch::setPluginParameter(QString prop, QVariant value)

engine/src/universe.cpp
@@ -1027,11 +1027,13 @@ void Universe::savePatchXML(
     QString profileName,
     QMap<QString, QVariant> parameters) const
 {
+    // sanity check: don't save invalid data
+    if (pluginName.isEmpty() || pluginName == KInputNone || line == QLCIOPlugin::invalidLine())
+        return;
+
     doc->writeStartElement(tag);
-    if (!pluginName.isEmpty() && pluginName != KInputNone)
-        doc->writeAttribute(KXMLQLCUniversePlugin, pluginName);
-    if (line != QLCIOPlugin::invalidLine())
-        doc->writeAttribute(KXMLQLCUniverseLine, QString::number(line));
+    doc->writeAttribute(KXMLQLCUniversePlugin, pluginName);
+    doc->writeAttribute(KXMLQLCUniverseLine, QString::number(line));
     if (!profileName.isEmpty() && profileName != KInputNone)
         doc->writeAttribute(KXMLQLCUniverseProfileName, profileName);
 