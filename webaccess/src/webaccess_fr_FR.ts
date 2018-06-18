114e9e4fa5f9650d0042a813ea0ee56b9fa2ff06
Nils Van Zuijlen <nils.van-zuijlen@mailo.com>
2018-06-18 20:01:15 +0200

    Added translations in webaccess_fr_FR.ts

--- webaccess/src/webaccess_fr_FR.ts
+++ webaccess/src/webaccess_fr_FR.ts
@@ -16,23 +16,23 @@
     <message>
         <location filename="webaccess.cpp" line="396"/>
         <source>Username and password are required fields.</source>
-        <translation type="unfinished"></translation>
+        <translation>Le nom d&apos;utilisateur et le mot de passe sont des champs requis.</translation>
     </message>
     <message>
         <location filename="webaccess.cpp" line="402"/>
         <location filename="webaccess.cpp" line="427"/>
         <source>User level has to be a positive integer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Le niveau de l&apos;utilisateur doit être un entier positif.</translation>
     </message>
     <message>
         <location filename="webaccess.cpp" line="421"/>
         <source>Username is required.</source>
-        <translation type="unfinished"></translation>
+        <translation>Un nom d&apos;utilisateur est requis.</translation>
     </message>
     <message>
         <location filename="webaccess.cpp" line="439"/>
         <source>Error while saving passwords file.</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur lors de la sauvegarde des mots de passe.</translation>
     </message>
     <message>
         <location filename="webaccess.cpp" line="454"/>
@@ -116,33 +116,33 @@
     <message>
         <location filename="webaccessconfiguration.cpp" line="221"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Nom d&apos;utilisateur</translation>
     </message>
     <message>
         <location filename="webaccessconfiguration.cpp" line="222"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Mot de passe</translation>
     </message>
     <message>
         <location filename="webaccessconfiguration.cpp" line="223"/>
         <source>Access level</source>
-        <translation type="unfinished"></translation>
+        <translation>Niveau d&apos;accès</translation>
     </message>
     <message>
         <location filename="webaccessconfiguration.cpp" line="224"/>
         <source>Action</source>
-        <translation type="unfinished"></translation>
+        <translation>Action</translation>
     </message>
     <message>
         <location filename="webaccessconfiguration.cpp" line="237"/>
         <source>Leave blank to not change</source>
-        <translation type="unfinished"></translation>
+        <translation>Laisser vide pour ne pas modifier</translation>
     </message>
     <message>
         <location filename="webaccessconfiguration.cpp" line="244"/>
         <location filename="webaccessconfiguration.cpp" line="274"/>
         <source>Only Virtual Console</source>
-        <translation type="unfinished"></translation>
+        <translation>Console Virtuelle Uniquement</translation>
     </message>
     <message>
         <location filename="webaccessconfiguration.cpp" line="249"/>
@@ -154,45 +154,45 @@
         <location filename="webaccessconfiguration.cpp" line="254"/>
         <location filename="webaccessconfiguration.cpp" line="278"/>
         <source>Everything</source>
-        <translation type="unfinished"></translation>
+        <translation>Tout</translation>
     </message>
     <message>
         <location filename="webaccessconfiguration.cpp" line="260"/>
         <location filename="webaccessconfiguration.cpp" line="285"/>
         <source>Change</source>
-        <translation type="unfinished"></translation>
+        <translation>Modifier</translation>
     </message>
     <message>
         <location filename="webaccessconfiguration.cpp" line="262"/>
         <location filename="webaccessconfiguration.cpp" line="285"/>
         <source>Delete user</source>
-        <translation type="unfinished"></translation>
+        <translation>Supprimer l&apos;utilisateur</translation>
     </message>
     <message>
         <location filename="webaccessconfiguration.cpp" line="268"/>
         <source>New username...</source>
-        <translation type="unfinished"></translation>
+        <translation>Nouveau nom d&apos;utilisateur...</translation>
     </message>
     <message>
         <location filename="webaccessconfiguration.cpp" line="269"/>
         <location filename="webaccessconfiguration.cpp" line="287"/>
         <source>New password...</source>
-        <translation type="unfinished"></translation>
+        <translation>Nouveau mot de passe...</translation>
     </message>
     <message>
         <location filename="webaccessconfiguration.cpp" line="286"/>
         <source>Username and password are required fields.</source>
-        <translation type="unfinished"></translation>
+        <translation>Le nom d&apos;utilisateur et le mot de passe sont des champs requis.</translation>
     </message>
     <message>
         <location filename="webaccessconfiguration.cpp" line="289"/>
         <source>Add user</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajouter l&apos;utilisateur</translation>
     </message>
     <message>
         <location filename="webaccessconfiguration.cpp" line="294"/>
         <source>Note: if there isn&apos;t at least one user with access level &quot;Everything&quot; on the list authorization will be disabled.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Note&#xa0;: s&apos;il n&apos;y a pas au moins un utilisateur avec le niveau d&apos;accès &quot;Tout&quot; dans la liste, l&apos;autorisation sera désactivée.</translation>
     </message>
     <message>
         <location filename="webaccessconfiguration.cpp" line="325"/>
@@ -222,7 +222,7 @@
     <message>
         <location filename="webaccessconfiguration.cpp" line="366"/>
         <source>Authorized users</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilisateurs autorisés</translation>
     </message>
 </context>
 <context>
