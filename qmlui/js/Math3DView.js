43cf6b4487c79c012c8d69ee3b96e9a2b436e6b4
Massimo Callegari <massimocallegari@yahoo.it>
2018-07-15 20:35:35 +0200

    codacy: a few syntax fixes

--- qmlui/js/Math3DView.js
+++ qmlui/js/Math3DView.js
@@ -19,7 +19,7 @@
 
 function perspective(fovy, aspect, zNear, zFar)
 {
-    var ymax = zNear * Math.tan(fovy)
+    var ymax = zNear * Math.tan(fovy);
     var xmax = ymax * aspect;
     var left = -xmax;
     var right = +xmax;
@@ -34,73 +34,70 @@ function perspective(fovy, aspect, zNear, zFar)
         f1 / f2, 0.0, 0.0, 0.0,
         0.0, f1 / f3, 0.0, 0.0,
         (right + left) / f2, (top + bottom) / f3, (-zFar - zNear) / f4, -1.0,
-        0.0, 0.0, (-zFar * f1) / f4, 0.0).transposed()
+        0.0, 0.0, (-zFar * f1) / f4, 0.0).transposed();
 }
 
 function getLightViewMatrix(lightMatrix, panRotation, tiltRotation, lightPos)
 {
     var m = Qt.matrix4x4();
-    m = m.times(lightMatrix)
+    m = m.times(lightMatrix);
     m.rotate(panRotation, Qt.vector3d(0, 1, 0));
     m.rotate(tiltRotation, Qt.vector3d(1, 0, 0));
 
     // extract the axes of our view matrix.
-    var xb = (m.times(Qt.vector4d(1.0, 0.0, 0.0, 0.0))).toVector3d()
-    var yb = (m.times(Qt.vector4d(0.0, 1.0, 0.0, 0.0))).toVector3d()
-    var zb = (m.times(Qt.vector4d(0.0, 0.0, 1.0, 0.0))).toVector3d()
-    var left = zb
-    var u = xb
-    var forward = yb
+    var xb = (m.times(Qt.vector4d(1.0, 0.0, 0.0, 0.0))).toVector3d();
+    var yb = (m.times(Qt.vector4d(0.0, 1.0, 0.0, 0.0))).toVector3d();
+    var zb = (m.times(Qt.vector4d(0.0, 0.0, 1.0, 0.0))).toVector3d();
+    var left = zb;
+    var u = xb;
+    var forward = yb;
+    var eye = lightPos;
 
-    var eye = lightPos
-
-    var vm =  Qt.matrix4x4(
+    return Qt.matrix4x4(
         left.x, u.x, forward.x, 0.0,
         left.y, u.y, forward.y, 0.0,
         left.z, u.z, forward.z, 0.0,
-        -left.dotProduct(eye), -u.dotProduct(eye), -forward.dotProduct(eye), 1.0).transposed()
-
-    return vm
+        -left.dotProduct(eye), -u.dotProduct(eye), -forward.dotProduct(eye), 1.0).transposed();
 }
 
 function getLightProjectionMatrix(distCutoff, coneBottomRadius, coneTopRadius, headLength, cutoffAngle)
 {
-    var d = distCutoff / (coneBottomRadius / coneTopRadius - 1.0)
-    var trans = -d + 0.5 * headLength
+    var d = distCutoff / (coneBottomRadius / coneTopRadius - 1.0);
+    var trans = -d + 0.5 * headLength;
 
-    var m = perspective(cutoffAngle, 1.0,  d, d + distCutoff).times(Qt.matrix4x4(
+    return perspective(cutoffAngle, 1.0,  d, d + distCutoff).times(Qt.matrix4x4(
                 1.0, 0.0, 0.0, 0.0,
                 0.0, 1.0, 0.0, 0.0,
                 0.0, 0.0, 1.0, trans,
-                0.0, 0.0, 0.0, 1.0))
-
-    return m
+                0.0, 0.0, 0.0, 1.0));
 }
 
 function lookAt(eye, center, up)
 {
     // compute the basis vectors.
-    var forward = (center.minus(eye)).normalized() // forward vector.
-    var left = (forward.crossProduct(up)).normalized() // left vector.
-    var u = (left.crossProduct(forward)).normalized() // up vector.
+    var forward = (center.minus(eye)).normalized(); // forward vector
+    var left = (forward.crossProduct(up)).normalized(); // left vector
+    var u = (left.crossProduct(forward)).normalized(); // up vector
 
     return Qt.matrix4x4(
         left.x, u.x, -forward.x, 0.0,
         left.y, u.y, -forward.y, 0.0,
         left.z, u.z, -forward.z, 0.0,
-        -left.dotProduct(eye), -u.dotProduct(eye), forward.dotProduct(eye), 1.0).transposed()
+        -left.dotProduct(eye), -u.dotProduct(eye), forward.dotProduct(eye), 1.0).transposed();
 }
 
 function getLightDirection(transform, panTransform, tiltTransform)
 {
-    var m = transform.matrix
-    if (panTransform)
-        m = transform.matrix.times(panTransform.matrix)
-    if (tiltTransform)
-        m = m.times(tiltTransform.matrix)
-    m = m.times(Qt.vector4d(0.0, -1.0, 0.0, 0.0))
+    var m = transform.matrix;
+    if (panTransform) {
+        m = transform.matrix.times(panTransform.matrix);
+    }
+    if (tiltTransform) {
+        m = m.times(tiltTransform.matrix);
+    }
+    m = m.times(Qt.vector4d(0.0, -1.0, 0.0, 0.0));
 
-    return (m.toVector3d().normalized())
+    return (m.toVector3d().normalized());
 }
 
 function getLightViewProjectionScaleOffsetMatrix(lightViewProjectionMatrix)
@@ -109,5 +106,5 @@ function getLightViewProjectionScaleOffsetMatrix(lightViewProjectionMatrix)
             0.5, 0.0, 0.0, 0.5,
             0.0, 0.5, 0.0, 0.5,
             0.0, 0.0, 0.5, 0.5,
-            0.0, 0.0, 0.0, 1.0).times(lightViewProjectionMatrix)
+            0.0, 0.0, 0.0, 1.0).times(lightViewProjectionMatrix);
 }
