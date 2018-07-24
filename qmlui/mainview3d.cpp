377d7a1876e6d89c38ae56a55b795d7cfc8612f7
Massimo Callegari <massimocallegari@yahoo.it>
2018-07-24 22:08:02 +0200

    qmlui: and cleanup

--- qmlui/mainview3d.cpp
+++ qmlui/mainview3d.cpp
@@ -627,7 +627,7 @@ QEntity *MainView3D::inspectEntity(QEntity *entity, SceneItem *meshRef,
             {             
                 material->addParameter(new QParameter("diffuse", pMaterial->diffuse()));
                 material->addParameter(new QParameter("specular", pMaterial->specular()));
-                material->addParameter(new QParameter("shininess", pMaterial->shininess() ? pMaterial->shininess() : 1.0));     
+                material->addParameter(new QParameter("shininess", pMaterial->shininess() ? pMaterial->shininess() : 1.0));
             }
             else
             {
