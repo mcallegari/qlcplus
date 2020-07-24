9b92b61c1fe308e32c4b6be0d1da03855ce8d9ec
Massimo Callegari <massimocallegari@yahoo.it>
2020-07-24 09:56:08 +0200

    ui/test: more font comparison fixes

--- ui/test/vccuelist/vccuelist_test.cpp
+++ ui/test/vccuelist/vccuelist_test.cpp
@@ -405,7 +405,7 @@ void VCCueList_Test::loadXML()
 
     QCOMPARE(cl.pos(), QPoint(3, 4));
     QCOMPARE(cl.size(), QSize(42, 69));
-    QCOMPARE(cl.font(), f);
+    QCOMPARE(cl.font().toString(), f.toString());
 
     cl.postLoad();
     QCOMPARE(cl.m_tree->topLevelItemCount(), 4);
