4551847e11d423f37b090ede20ca94c9369f54b3
Michał Kluska <kluska.mk@gmail.com>
2020-11-06 14:00:07 +0100

    Removed GoboRotatingMacro preset.

--- qmlui/mainview3d.cpp
+++ qmlui/mainview3d.cpp
@@ -1211,7 +1211,6 @@ void MainView3D::updateFixtureItem(Fixture *fixture, quint16 headIndex, quint16 
                 {
                     case QLCCapability::GoboMacro:
                     case QLCCapability::GoboShakeMacro:
-                    case QLCCapability::GoboRotatingMacro:
                     {
                         QString resName = cap->resource(0).toString();
 
