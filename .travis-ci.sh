07dd6a4e8b46adfcf6bfa7ee11f1325b25bf87e9
Massimo Callegari <massimocallegari@yahoo.it>
2021-04-11 13:33:45 +0200

    travis: use a single script for env and build

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,16 +3,45 @@
 # This script is triggered from the script section of .travis.yml
 # It runs the appropriate commands depending on the task requested.
 
+if [ "$QT" = "qt5qml" ]; then
+  export QMAKE=/opt/qt514/bin/qmake
+else
+  export QMAKE=/opt/qt56/bin/qmake
+
+  # Remove the old g++/gcc to ensure we're using the latest ones
+  if [ "$CXX" = "g++" ]; then
+    sudo rm /usr/bin/gcc
+    sudo rm /usr/bin/g++
+    sudo ln -s /usr/bin/gcc-7 /usr/bin/gcc
+    sudo ln -s /usr/bin/g++-7 /usr/bin/g++
+    export CXX="g++-7" CC="gcc-7"
+  fi
+
+  # Start xvfb so UI tests don't fail
+  export DISPLAY=:99.0
+  /etc/init.d/xvfb start
+fi
+
+if [ "$TASK" = "coverage" ]; then
+  gem install coveralls-lcov
+fi
+
+# Report the compiler version
+$CXX --version
+
+# Report the qmake version
+$QMAKE -v
+
 # Otherwise compile and check as normal
 if [ "$TASK" = "compile" ]; then
   if [ "$QT" = "qt5" ]; then
     $QMAKE QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX && make && ./unittest.sh
-    exit $?  
+    exit $?
   fi
   if [ "$QT" = "qt5qml" ]; then
     $QMAKE QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX CONFIG+=qmlui && make
-    exit $?  
-  fi  
+    exit $?
+  fi
 fi
 if [ "$TASK" = "coverage" ]; then
 $QMAKE CONFIG+=coverage QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX && make && ./coverage.sh
