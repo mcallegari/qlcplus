c7928d453d43b73a028042f39df1633cf8e9f8e4
Hans-Jürgen Tappe <hj.tappe@web.de>
2021-06-12 11:38:25 +0200

    Fix path comparison: absolute path does not contain relative path (../).

--- engine/test/qlcfixturedefcache/qlcfixturedefcache_test.cpp
+++ engine/test/qlcfixturedefcache/qlcfixturedefcache_test.cpp
@@ -200,7 +200,9 @@ void QLCFixtureDefCache_Test::defDirectories()
     QDir dir = QLCFixtureDefCache::systemDefinitionDirectory();
     QVERIFY(dir.filter() & QDir::Files);
     QVERIFY(dir.nameFilters().contains(QString("*%1").arg(KExtFixture)));
-    QVERIFY(dir.absolutePath().contains(FIXTUREDIR));
+    QDir fxDir;
+    fxDir.setPath(FIXTUREDIR);
+    QCOMPARE(dir.absolutePath(), fxDir.absolutePath());
 
     dir = QLCFixtureDefCache::userDefinitionDirectory();
 #ifndef SKIP_TEST
