4d89d85d94c410a6aae53e9b0cabbd39c16ceb9b
Hans-Jürgen Tappe <hj.tappe@web.de>
2021-06-13 12:20:15 +0200

    Run the tests throuch "make check".i
    Run them in both configurations as they address a different set of
    tests, contain specific test assumptions and may lead to different
    results.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -31,11 +31,11 @@ $QMAKE -v
 # Otherwise compile and check as normal
 if [ "$TASK" = "compile" ]; then
   if [ "$QT" = "qt5" ]; then
-    $QMAKE QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX && make && ./unittest.sh
+    $QMAKE QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX && make && make check
     exit $?
   fi
   if [ "$QT" = "qt5qml" ]; then
-    $QMAKE QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX CONFIG+=qmlui && make
+    $QMAKE QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX CONFIG+=qmlui && make && make check
     exit $?
   fi
 fi
