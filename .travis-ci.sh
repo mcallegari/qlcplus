cab314de3b1fd524ac8176f15539fe0312012f4c
Massimo Callegari <massimocallegari@yahoo.it>
2021-10-03 18:27:29 +0200

    travis: fix build script too

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,20 +3,7 @@
 # This script is triggered from the script section of .travis.yml
 # It runs the appropriate commands depending on the task requested.
 
-if [ "$QT" = "qt5qml" ]; then
-  export QMAKE=/opt/qt514/bin/qmake
-else
-  export QMAKE=/opt/qt56/bin/qmake
-
-  # Remove the old g++/gcc to ensure we're using the latest ones
-  if [ "$CXX" = "g++" ]; then
-    sudo rm /usr/bin/gcc
-    sudo rm /usr/bin/g++
-    sudo ln -s /usr/bin/gcc-7 /usr/bin/gcc
-    sudo ln -s /usr/bin/g++-7 /usr/bin/g++
-    export CXX="g++-7" CC="gcc-7"
-  fi
-fi
+export QMAKE=/opt/qt514/bin/qmake
 
 if [ "$TASK" = "coverage" ]; then
   gem install coveralls-lcov
