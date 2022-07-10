43945267f4c5318f736a087a170e20eab7aeb3c2
Massimo Callegari <massimocallegari@yahoo.it>
2022-07-10 20:17:29 +0200

    plugins/hid: chore info format

--- plugins/hid/hiddmxdevice.cpp
+++ plugins/hid/hiddmxdevice.cpp
@@ -116,7 +116,7 @@ QString HIDDMXDevice::infoText()
 {
     QString info;
 
-    info += QString("<B>%1</B><P>").arg(m_name);
+    info += QString("<H3>%1</H3><P>").arg(m_name);
 
     return info;
 }
