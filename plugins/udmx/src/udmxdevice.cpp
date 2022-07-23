df7f8d92c06a3fa398d2623a2834633370fd365b
Massimo Callegari <massimocallegari@yahoo.it>
2022-07-23 15:10:01 +0200

    plugins/udmx: fix bad array copy
    Reported here: https://www.qlcplus.org/forum/viewtopic.php?f=32&t=15658

--- plugins/udmx/src/udmxdevice.cpp
+++ plugins/udmx/src/udmxdevice.cpp
@@ -207,7 +207,8 @@ const struct libusb_device* UDMXDevice::device() const
 
 void UDMXDevice::outputDMX(const QByteArray& universe)
 {
-    m_universe.replace(0, qMin(universe.size(), m_universe.size()), universe.constData());
+    m_universe.replace(0, qMin(universe.size(), m_universe.size()), universe.constData(),
+                          qMin(universe.size(), m_universe.size()));
 }
 
 void UDMXDevice::stop()
