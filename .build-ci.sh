cf5864c481e02d73dcce7d57fb07a72e7a59a5c8
Hans-Jürgen Tappe <hj.tappe@web.de>
2022-10-23 21:49:48 +0200

    Make use of available CPUs

--- .build-ci.sh
+++ .build-ci.sh
@@ -18,18 +18,21 @@ $CXX --version
 # Report the qmake version
 $QMAKE -v
 
+# Make use of available processors
+NPROC=$(nproc)
+
 # Otherwise compile and check as normal
 if [ "$TASK" = "compile" ]; then
   if [ "$QT" = "qt5" ]; then
-    $QMAKE QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX && make && make check
+    $QMAKE QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX && make -j $NPROC && make check
     exit $?
   fi
   if [ "$QT" = "qt5qml" ]; then
-    $QMAKE QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX CONFIG+=qmlui && make && make check
+    $QMAKE QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX CONFIG+=qmlui && make -j $NPROC && make check
     exit $?
   fi
 fi
 if [ "$TASK" = "coverage" ]; then
-  $QMAKE CONFIG+=coverage QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX && make && make lcov
+  $QMAKE CONFIG+=coverage QMAKE_CXX=$CXX QMAKE_CC=$CC QMAKE_LINK=$CXX QMAKE_LINK_SHLIB=$CXX && make -j $NPROC && make lcov
   exit $?
 fi
