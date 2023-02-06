mcallegari/qlcplus

commit da8b623f468454dbe957940770f84bbb17aa83c2

linux: update a few dependencies

platforms/linux/qlcplus.spec
@@ -4,19 +4,27 @@ Summary: Q Light Controller Plus - The free DMX lighting console
 License: Apache License, Version 2.0
 Name: qlcplus
 Version: %{version}
-BuildRequires: gcc-c++ pkg-config
-BuildRequires: libusb-devel, libudev-devel
-BuildRequires: alsa-lib-devel >= 1.0.23, libsndfile-devel, libmad-devel, fftw-devel >= 3.0.0
-#BuildRequires: libola-devel
-BuildRequires: desktop-file-utils
+BuildRequires:  desktop-file-utils
+BuildRequires:  fdupes
+BuildRequires:  gcc-c++
+BuildRequires:  pkgconfig
+BuildRequires:  pkgconfig(Qt5Multimedia)
+BuildRequires:  pkgconfig(Qt5Script)
+BuildRequires:  pkgconfig(Qt5Widgets)
+BuildRequires:  pkgconfig(alsa)
+BuildRequires:  pkgconfig(fftw3)
+BuildRequires:  pkgconfig(libftdi1)
+BuildRequires:  pkgconfig(libola)
+BuildRequires:  pkgconfig(libudev)
+BuildRequires:  pkgconfig(libusb1)
+BuildRequires:  pkgconfig(mad)
+BuildRequires:  pkgconfig(sndfile)
 %if %{defined fedora}
-BuildRequires: libftdi-devel
-BuildRequires: qt5-qtbase-devel, qt5-qtmultimedia-devel, qt5-qtscript-devel, qt5-linguist
-%endif
-%if %{defined suse_version}
-BuildRequires: libftdi1-devel
-BuildRequires: libqt5-qtbase-devel, libqt5-qtmultimedia-devel, libqt5-qtscript-devel, libqt5-linguist
-%endif
+BuildRequires:  qt5-linguist
+BuildRequires:  qt5-qtconfiguration-devel
+%else
+BuildRequires:  libqt5-linguist-devel
+BuildRequires:  update-desktop-files
 Release: 1
 Source: qlcplus-%{version}.tar.gz
 URL: https://www.qlcplus.org/

debian/compat
@@ -1,1 +1,1 @@
-7
+10

debian/control
@@ -7,13 +7,13 @@ Build-Depends:
  g++,
  libasound2-dev (>= 1.0.16),
  libfftw3-dev,
- libftdi-dev (>= 0.17),
+ libftdi1-dev (>= 0.17),
  libmad0-dev,
  qtbase5-dev, qtscript5-dev, qtmultimedia5-dev,
  libsndfile1-dev,
  libstdc++-dev,
  libudev-dev,
- libusb-dev (>= 2:0.1.12),
+ libusb-1.0-0-dev (>= 2:0.1.12),
  make,
  pkg-config,
  libxml2-utils,

commit 2b81f5cb5388956daaca3f3f62db0359dd316dab

linux: Fix missing %endif in rpm spec
Problem introduced by da8b623f468454dbe957940770f84bbb17aa83c2

platforms/linux/qlcplus.spec
@@ -25,6 +25,7 @@ BuildRequires:  qt5-qtconfiguration-devel
 %else
 BuildRequires:  libqt5-linguist-devel
 BuildRequires:  update-desktop-files
+%endif
 Release: 1
 Source: qlcplus-%{version}.tar.gz
 URL: https://www.qlcplus.org/