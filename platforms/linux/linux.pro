mcallegari/qlcplus

commit da8b623f468454dbe957940770f84bbb17aa83c2

linux: update a few dependencies

platforms/linux/qlcplus.spec
@@ -4,19 +4,27 @@ Summary: Q Light Controller Plus - The free DMX lighting console
 License: Apache License, Version 2.0
 Name: qlcplus
 Version: %{version}
-BuildRequires: gcc-c++ pkg-config
-BuildRequires: libusb-devel, libudev-devel
-BuildRequires: alsa-lib-devel >= 1.0.23, libsndfile-devel, libmad-devel, fftw-devel >= 3.0.0
-#BuildRequires: libola-devel
-BuildRequires: desktop-file-utils
+BuildRequires:  desktop-file-utils
+BuildRequires:  fdupes
+BuildRequires:  gcc-c++
+BuildRequires:  pkgconfig
+BuildRequires:  pkgconfig(Qt5Multimedia)
+BuildRequires:  pkgconfig(Qt5Script)
+BuildRequires:  pkgconfig(Qt5Widgets)
+BuildRequires:  pkgconfig(alsa)
+BuildRequires:  pkgconfig(fftw3)
+BuildRequires:  pkgconfig(libftdi1)
+BuildRequires:  pkgconfig(libola)
+BuildRequires:  pkgconfig(libudev)
+BuildRequires:  pkgconfig(libusb1)
+BuildRequires:  pkgconfig(mad)
+BuildRequires:  pkgconfig(sndfile)
 %if %{defined fedora}
-BuildRequires: libftdi-devel
-BuildRequires: qt5-qtbase-devel, qt5-qtmultimedia-devel, qt5-qtscript-devel, qt5-linguist
-%endif
-%if %{defined suse_version}
-BuildRequires: libftdi1-devel
-BuildRequires: libqt5-qtbase-devel, libqt5-qtmultimedia-devel, libqt5-qtscript-devel, libqt5-linguist
-%endif
+BuildRequires:  qt5-linguist
+BuildRequires:  qt5-qtconfiguration-devel
+%else
+BuildRequires:  libqt5-linguist-devel
+BuildRequires:  update-desktop-files
 Release: 1
 Source: qlcplus-%{version}.tar.gz
 URL: https://www.qlcplus.org/

debian/compat
@@ -1,1 +1,1 @@
-7
+10

debian/control
@@ -7,13 +7,13 @@ Build-Depends:
  g++,
  libasound2-dev (>= 1.0.16),
  libfftw3-dev,
- libftdi-dev (>= 0.17),
+ libftdi1-dev (>= 0.17),
  libmad0-dev,
  qtbase5-dev, qtscript5-dev, qtmultimedia5-dev,
  libsndfile1-dev,
  libstdc++-dev,
  libudev-dev,
- libusb-dev (>= 2:0.1.12),
+ libusb-1.0-0-dev (>= 2:0.1.12),
  make,
  pkg-config,
  libxml2-utils,

commit 9175c1b45a1c938b360f7e27172b428df86b36e5

Minor linux changes while looking at qml mode (#1388)
* linux: Fix missing %endif in rpm spec

Problem introduced by da8b623f468454dbe957940770f84bbb17aa83c2

* linux: Fedora has pkgconfig(libusb-1.0), not pkgconfig(libusb1)

* linux: there are no qlcplus-qml man pages

* linux: there is no separate qml fixture editor

* linux: prepare for building rpms with qmlui

platforms/linux/linux.pro
@@ -4,11 +4,17 @@ TEMPLATE = subdirs
 TARGET   = icons
 
 desktop.path   = $$INSTALLROOT/share/applications/
-desktop.files += qlcplus.desktop qlcplus-fixtureeditor.desktop
+desktop.files += qlcplus.desktop
+!qmlui: {
+    desktop.files += qlcplus-fixtureeditor.desktop
+}
 INSTALLS      += desktop
 
 icons.path   = $$INSTALLROOT/share/pixmaps/
-icons.files += ../../resources/icons/png/qlcplus.png ../../resources/icons/png/qlcplus-fixtureeditor.png
+icons.files += ../../resources/icons/png/qlcplus.png
+!qmlui: {
+    icons.files += ../../resources/icons/png/qlcplus-fixtureeditor.png
+}
 INSTALLS    += icons
 
 mime.path   = $$INSTALLROOT/share/mime/packages
@@ -16,12 +22,17 @@ mime.files += qlcplus.xml
 INSTALLS   += mime
 
 appdata.path   = $$METAINFODIR
-appdata.files += org.qlcplus.QLCPlus.appdata.xml org.qlcplus.QLCPlusFixtureEditor.appdata.xml
+appdata.files += org.qlcplus.QLCPlus.appdata.xml
+!qmlui: {
+    appdata.files += org.qlcplus.QLCPlusFixtureEditor.appdata.xml
+}
 INSTALLS      += appdata
 
+!qmlui: {
 manpages.path = $$INSTALLROOT/$$MANDIR
 manpages.files += *.1
 INSTALLS += manpages
+}
 
 # This is nowadays run by dpkg (TODO: rpm)
 #MIMEUPDATE    = $$system("which update-mime-database")

platforms/linux/qlcplus.spec
@@ -1,4 +1,5 @@
 %define version %(echo $QLCPLUS_VERSION)
+#define ui qmlui
 
 Summary: Q Light Controller Plus - The free DMX lighting console
 License: Apache License, Version 2.0
@@ -16,15 +17,21 @@ BuildRequires:  pkgconfig(fftw3)
 BuildRequires:  pkgconfig(libftdi1)
 BuildRequires:  pkgconfig(libola)
 BuildRequires:  pkgconfig(libudev)
-BuildRequires:  pkgconfig(libusb1)
 BuildRequires:  pkgconfig(mad)
 BuildRequires:  pkgconfig(sndfile)
 %if %{defined fedora}
+BuildRequires:  pkgconfig(libusb-1.0)
 BuildRequires:  qt5-linguist
 BuildRequires:  qt5-qtconfiguration-devel
+%if "%{ui}" == "qmlui"
+BuildRequires:  qt5-qt3d-devel
+BuildRequires:  qt5-qtsvg-devel
+%endif
 %else
+BuildRequires:  pkgconfig(libusb1)
 BuildRequires:  libqt5-linguist-devel
 BuildRequires:  update-desktop-files
+%endif
 Release: 1
 Source: qlcplus-%{version}.tar.gz
 URL: https://www.qlcplus.org/
@@ -51,9 +58,14 @@ sed -ie '/UDEVRULESDIR/s|/etc/udev/rules.d|/usr/lib/udev/rules.d|' variables.pri
 
 %build
 # qmake-qt5 will only include existing files in install_translations - create the .qm files first
-./translate.sh ui
 
-qmake-qt5
+%if "%{ui}" == "qmlui"
+    ./translate.sh qmlui
+    qmake-qt5 CONFIG+=qmlui
+%else
+    ./translate.sh ui
+    qmake-qt5
+%endif
 make %{?_smp_mflags}
 
 #############################################################################
@@ -62,6 +74,10 @@ make %{?_smp_mflags}
 
 %install
 INSTALL_ROOT=$RPM_BUILD_ROOT make install
+%if "%{ui}" == "qmlui"
+mv %{buildroot}/%{_bindir}/qlcplus-qml %{buildroot}/%{_bindir}/qlcplus
+sed -i -e 's/Exec=qlcplus --open %f/Exec=qlcplus/g' %{buildroot}/%{_datadir}/applications/qlcplus.desktop
+%endif
 
 desktop-file-validate %{buildroot}/%{_datadir}/applications/*.desktop
 
@@ -81,8 +97,10 @@ desktop-file-validate %{buildroot}/%{_datadir}/applications/*.desktop
 %files
 %{_bindir}/*
 %{_libdir}/libqlcplusengine.so*
+%if "%{ui}" != "qmlui"
 %{_libdir}/libqlcplusui.so*
 %{_libdir}/libqlcpluswebaccess.so*
+%endif
 %dir %{_datadir}/qlcplus
 %{_datadir}/applications/*
 %{_datadir}/metainfo/*
@@ -96,7 +114,12 @@ desktop-file-validate %{buildroot}/%{_datadir}/applications/*.desktop
 %{_datadir}/qlcplus/modifierstemplates
 %{_datadir}/qlcplus/rgbscripts
 %{_datadir}/qlcplus/translations
+%if "%{ui}" == "qmlui"
+%{_datadir}/qlcplus/colorfilters
+%{_datadir}/qlcplus/meshes
+%else
 %{_datadir}/qlcplus/web
+%endif
 %_libdir/qt5/plugins/qlcplus/audio/libmadplugin.so
 %_libdir/qt5/plugins/qlcplus/audio/libsndfileplugin.so
 %_libdir/qt5/plugins/qlcplus/libartnet.so
@@ -112,8 +135,10 @@ desktop-file-validate %{buildroot}/%{_datadir}/applications/*.desktop
 %_libdir/qt5/plugins/qlcplus/libpeperoni.so
 %_libdir/qt5/plugins/qlcplus/libspi.so
 %_libdir/qt5/plugins/qlcplus/libudmx.so
+%if "%{ui}" != "qmlui"
 %_mandir/*/*
 %doc /usr/share/qlcplus/documents
+%endif
 /usr/lib/udev/rules.d/z65-anyma-udmx.rules
 /usr/lib/udev/rules.d/z65-dmxusb.rules
 /usr/lib/udev/rules.d/z65-fx5-hid.rules