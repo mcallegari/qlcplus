301c3b7b443d0ecb0c46f984709d53c88e30ee46
Hans-Jürgen Tappe <hj.tappe@web.de>
2023-04-15 22:06:20 +0200

    Rotate color array and format the algorithm.
    The algorithm prefers the inner colors (1-2-3-4-5) over the outer colors as the
    noise function output is a Gauss distribution. To make the colors set from thr
    outside more persent, shift them from 1 and 2 to 2 and 3.
    
    Also, format the algorithms.

--- resources/rgbscripts/plasmacolors.js
+++ resources/rgbscripts/plasmacolors.js
@@ -168,6 +168,7 @@ function()
     algo.color5Index = algo.setColor(4, _preset);
     util.initialized = false;
   };
+
   algo.getColor5 = function()
   {
     return algo.getColor(4);
@@ -178,6 +179,7 @@ function()
     algo.presetSize = _size;
     util.initialized = false;
   };
+
   algo.getSize = function()
   {
     return algo.presetSize;
@@ -188,6 +190,7 @@ function()
     algo.ramp = _ramp;
     util.initialized = false;
   };
+
   algo.getRamp = function()
   {
     return algo.ramp;
@@ -198,6 +201,7 @@ function()
     algo.stepsize = _step;
     util.initialized = false;
   };
+
   algo.getStep = function()
   {
     return algo.stepsize;
@@ -212,6 +216,13 @@ function()
     for (var i = 0; i < algo.colorIndex.length; i++) {
       util.colorArray[i + algo.acceptColors] = colorPalette.collection[algo.colorIndex[i]][1];
     }
+    // Shift colors 1 & 2 as the outer colors are selected less in the Gauss-Noise function
+    var tempColor = util.colorArray[util.colorArray.length - 1];
+    for (var i = util.colorArray.length - 1; i > 0; i--) {
+      util.colorArray[i] = util.colorArray[i - 1];
+    }
+    util.colorArray[0] = tempColor;
+
     // calculate the gradient for the selected preset
     // with the given width
     var gradIdx = 0;
@@ -219,9 +230,9 @@ function()
     for (var i = 0; i < util.colorArray.length; i++)
     {
       var sColor = util.colorArray[i];
-      var eColor = util.colorArray[i + 1];
-      if (eColor == undefined) {
-        eColor = util.colorArray[0];
+      var eColor = util.colorArray[0];;
+      if (i < util.colorArray.length - 1) {
+        eColor = util.colorArray[i + 1];
       }
       util.gradientData[gradIdx++] = sColor;
       var sr = (sColor >> 16) & 0x00FF;
@@ -247,69 +258,119 @@ function()
     util.initialized = true;
   };
 
-  function fade(t) { return t * t * t * (t * (t * 6 - 15) + 10); }
-  function lerp( t, a, b) { return a + t * (b - a); }
-  function grad(hash, x, y, z) {
-    var h = hash & 15;                   // CONVERT LO 4 BITS OF HASH CODE
-    var u = h<8 ? x : y,                 // INTO 12 GRADIENT DIRECTIONS.
-      v = h<4 ? y : h===12||h===14 ? x : z;
-    return ((h&1) === 0 ? u : -u) + ((h&2) === 0 ? v : -v);
-  }
-  function scale(n) { return (1 + n)/2; }
-
   // This is a port of Ken Perlin's Java code. The
   // original Java code is at http://cs.nyu.edu/%7Eperlin/noise/.
   // Note that in this version, a number from 0 to 1 is returned.
   util.noise = function(x, y, z)
   {
     var p = new Array(512);
-    var permutation = [ 151,160,137,91,90,15,131,13,201,95,96,53,194,233, 7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,190, 6,148,247,120,234,75, 0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,88,237,149,56,87,174,20, 125,136,171,168,68,175,74,165,71,134,139,48,27,166,77,146,158,231,83,111,229, 122,60,211,133,230,220,105,92,41,55,46,245,40,244,102,143,54, 65,25,63,161,1, 216,80,73,209,76,132,187,208,89,18,169,200,196,135,130,116,188,159,86,164,100, 109,198,173,186,3,64,52,217,226,250,124,123,5,202,38,147,118,126,255,82,85,212, 207,206,59,227,47,16,58,17,182,189,28,42,223,183,170,213,119,248,152, 2,44,154, 163,70,221,153,101,155,167,43,172,9,129,22,39,253,19,98,108,110,79,113,224,232, 178,185,112,104,218,246,97,228,251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,49,192,214, 31,181,199,106,157,184, 84,204,176,115, 121,50,45,127,4,150,254,138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180];
+    var permutation = [ // 256 members
+      151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225,
+      140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247,
+      120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57,
+      177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74,
+      165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122,
+      60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54,
+      65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169,
+      200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3,
+      64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85,
+      212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170,
+      213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43,
+      172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185,
+      112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191,
+      179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31,
+      181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150,
+      254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195,
+      78, 66, 215, 61, 156, 180
+    ];
 
-    for (var i=0; i < 256 ; i++) {
-      p[256+i] = p[i] = permutation[i];
+    // initialize symmetrical permutation array(512)
+    for (var i = 0; i < 256; i++) {
+      p[i] = permutation[i];
+      p[256 + i] = permutation[i];
     }
-    var X = Math.floor(x) & 255,                  // FIND UNIT CUBE THAT
-      Y = Math.floor(y) & 255,                  // CONTAINS POINT.
-      Z = Math.floor(z) & 255;
-      x -= Math.floor(x);                       // FIND RELATIVE X,Y,Z
-      y -= Math.floor(y);                       // OF POINT IN CUBE.
-      z -= Math.floor(z);
-    var u = fade(x),                              // COMPUTE FADE CURVES
-      v = fade(y),                              // FOR EACH OF X,Y,Z.
-      w = fade(z);
-    var A = p[X  ]+Y, AA = p[A]+Z, AB = p[A+1]+Z, // HASH COORDINATES OF
-      B = p[X+1]+Y, BA = p[B]+Z, BB = p[B+1]+Z; // THE 8 CUBE CORNERS,
+    // Find unit cube that contains point.
+    var X = Math.floor(x) & 255;
+    var Y = Math.floor(y) & 255;
+    var Z = Math.floor(z) & 255;
+    // Find relative x,y,z of point in cube.
+    x -= Math.floor(x);
+    y -= Math.floor(y);
+    z -= Math.floor(z);
+    // Compute fade curves for each of y, y, z
+    var u = fade(x);
+    var v = fade(y);
+    var w = fade(z);
+    // Hash coordinates of the 8 cube corners,
+    var A  = p[X] + Y;
+    var AA = p[A] + Z;
+    var AB = p[A + 1] + Z;
+    var B  = p[X + 1] + Y;
+    var BA = p[B] + Z;
+    var BB = p[B + 1] + Z;
 
-    return scale(lerp(w, lerp(v, lerp(u, grad(p[AA  ], x  , y  , z   ),  // AND ADD
-                      grad(p[BA  ], x-1, y  , z   )),    // BLENDED
-              lerp(u, grad(p[AB  ], x  , y-1, z   ),     // RESULTS
-                      grad(p[BB  ], x-1, y-1, z   ))),   // FROM  8
-      lerp(v, lerp(u, grad(p[AA+1], x  , y  , z-1 ),     // CORNERS
-                      grad(p[BA+1], x-1, y  , z-1 )),    // OF CUBE
-              lerp(u, grad(p[AB+1], x  , y-1, z-1 ),
-                      grad(p[BB+1], x-1, y-1, z-1 )))));
+    // And add blended results from8 corners of cube
+    var rawNoise = lerp(w,
+      lerp(v, lerp(u, grad(p[AA    ], x  , y    , z    ),
+                      grad(p[BA    ], x-1, y    , z    )),
+              lerp(u, grad(p[AB    ], x  , y - 1, z    ),
+                      grad(p[BB    ], x-1, y - 1, z    ))),
+      lerp(v, lerp(u, grad(p[AA + 1], x  , y    , z - 1),
+                      grad(p[BA + 1], x-1, y    , z - 1)),
+              lerp(u, grad(p[AB + 1], x  , y - 1, z - 1),
+                      grad(p[BB + 1], x-1, y - 1, z - 1)))
+    );
+    // Scale to range between 0 and 1
+    var noise = scale(rawNoise);
+    return noise;
   };
+  function fade(t)
+  {
+    return t * t * t * (t * (t * 6 - 15) + 10);
+  }
+  function lerp(t, a, b)
+  {
+    return a + t * (b - a);
+  }
+  function grad(hash, x, y, z)
+  {
+    // CONVERT TO 4 BITS OF HASH CODE
+    var h = hash & 0x0000000f;
+    // INTO 12 GRADIENT DIRECTIONS.
+    var u = (h < 8) ? x : y;
+    var v = (h < 4) ? y : (h === 12 || h === 14) ? x : z;
+    return (((h & 1) === 0) ? u : -1 * u) + (((h & 2) === 0) ? v : -1 * v);
+  }
+  function scale(n)
+  {
+    var scaled = (1 + n) / 2;
+      scaled = Math.max(0, scaled - 0.3);
+    return scaled;
+  }
+
 
   algo.rgbMap = function(width, height, rgb, step, rawColors)
   {
     if (util.colorArray.length == 0) {
       util.initialized = false;
     } else if (util.colorArray.length >= algo.acceptColors - 1 && Array.isArray(rawColors)) {
+      // Check if the externally provided color has changed.
       for (var i = 0; i < Math.min(algo.acceptColors, rawColors.length); i++) {
         if (util.colorArray[i] !== rawColors[i]) {
           util.initialized = false;
         }
       }
     }
-    if (util.initialized === false) {
+    if (util.initialized === false)
+    {
       util.initialize(rawColors);
     }
 
-    var size = algo.presetSize/2;  // set a scaling value
+    var size = algo.presetSize / 2;  // set a scaling value
     var speed = Math.pow(100 , (algo.stepsize / 50)); // create a more uniform speed control
     algo.bstepcount += (speed / 500);
     algo.bstepcount = (algo.bstepcount % 256);  // A rolling step count for the noise function
-    var square = width>height ? width : height; // keep the patten square
+    var square = (width > height) ? width : height; // keep the patten square
 
     var map = new Array(height);
     for (var y = 0; y < height; y++)
@@ -320,8 +381,8 @@ function()
       {
         var nx = x / square;  // Normalize nx & ny to 0 - 1
         var ny = y / square;
-        var n = util.noise( size*nx, size*ny, algo.bstepcount);
-        var gradStep = Math.round( Math.pow(n , (algo.ramp/10)) * util.gradientData.length);
+        var n = util.noise(size * nx, size * ny, algo.bstepcount);
+        var gradStep = Math.round(Math.pow(n, (algo.ramp / 10)) * util.gradientData.length);
         map[y][x] = util.gradientData[gradStep];
       }
     }
@@ -331,7 +392,7 @@ function()
 
   algo.rgbMapStepCount = function(width, height)
   {
-    return 2;  // This make no difference to the script ;-)
+    return 2; // This make no difference to the script ;-)
   };
 
   // Development tool access
