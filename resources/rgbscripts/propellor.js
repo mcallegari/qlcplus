e0e3b53bd9a61c3134328d3b20beb67c2ab57f7c
yestalgia <42596763+yestalgia@users.noreply.github.com>
2023-07-08 15:08:41 +1000

    Fix: Efficiency improvements to the script

--- resources/rgbscripts/propellor.js
+++ resources/rgbscripts/propellor.js
@@ -18,89 +18,78 @@
 
   
 */
+(function () {
+  const algo = {};
+  algo.apiVersion = 2;
+  algo.name = "Propellor";
+  algo.author = "Christoph Klenge";
+  algo.acceptColors = 2;
 
-// Development tool access
-var testAlgo;
+  algo.properties = [];
+  algo.barWidth = 2;
+  algo.properties.push("name:barWidth|type:range|display:Bar Width|values:1,8|write:setBarWidth|read:getBarWidth");
+  algo.direction = 1;
+  algo.properties.push("name:direction|type:list|display:Direction|values:Clockwise,Counterclockwise|write:setDirection|read:getDirection");
+  algo.centerGap = 0;
+  algo.properties.push("name:centerGap|type:range|display:Center Gap|values:0,8|write:setCenterGap|read:getCenterGap");
 
-(
-  function()
-  {
-    var algo = new Object;
-    algo.apiVersion = 2;
-    algo.name = "Propellor";
-    algo.author = "Christoph Klenge";
-    algo.acceptColors = 2;
-
-    algo.properties = new Array();
-    algo.barWidth = 2;
-    algo.properties.push("name:barWidth|type:range|display:Bar Width|values:1,8|write:setBarWidth|read:getBarWidth");
-    algo.direction = 1;
-    algo.properties.push("name:direction|type:list|display:Direction|values:Clockwise,Counterclockwise|write:setDirection|read:getDirection");
-    algo.centerGap = 0;
-    algo.properties.push("name:centerGap|type:range|display:Center Gap|values:0,8|write:setCenterGap|read:getCenterGap");
-    	
-  algo.setBarWidth = function(barWidth)
-    {
+  algo.setBarWidth = function (barWidth) {
       algo.barWidth = barWidth;
-    };
-  algo.getBarWidth = function()
-    {
+  };
+  algo.getBarWidth = function () {
       return algo.barWidth;
-    };
+  };
 
-  algo.setDirection = function(direction)
-    {
-      if (direction == "Clockwise"){algo.direction = 1;}
-      else if (direction == "Counterclockwise"){algo.direction = -1;}
-      else {algo.direction = 1;}
-    };
-  algo.getDirection = function()
-    {
-      if (direction == 1){return"Clockwise";}
-      else if (direction == -1){return"Counterclockwise";}
+  algo.setDirection = function (direction) {
+      if (direction === "Clockwise") { algo.direction = 1; }
+      else if (direction === "Counterclockwise") { algo.direction = -1; }
+      else { algo.direction = 1; }
+  };
+  algo.getDirection = function () {
+      if (algo.direction === 1) { return "Clockwise"; }
+      else if (algo.direction === -1) { return "Counterclockwise"; }
       return "none";
-    };
+  };
 
-  algo.setCenterGap = function(centerGap)
-    {
+  algo.setCenterGap = function (centerGap) {
       algo.centerGap = centerGap;
-    };
-  algo.getCenterGap = function()
-    {
+  };
+  algo.getCenterGap = function () {
       return algo.centerGap;
-    };
-  
+  };
 
-  algo.rgbMap = function(width, height, rgb, step)
-    {   
-      var map = new Array(height);
-      for (var y = 0; y < height; y++)
-      {
-          map[y] = new Array();
-          for (var x = 0; x < width; x++)
-          {
-            angle = algo.direction*step*(2*3.14)/algo.rgbMapStepCount(width, height)
-            verticalOffset = (height-1)/2-y
-            horizontalOffset = (width-1)/2-x
-            distance = Math.sqrt(horizontalOffset*horizontalOffset+ verticalOffset*verticalOffset)
-            normalDistance = Math.abs(horizontalOffset*Math.cos(angle)+verticalOffset*Math.sin(angle))
-            if (normalDistance <= algo.barWidth/2 && distance >= algo.centerGap/2)
-              map[y][x] = rgb;
-            else 
-              map[y][x]  = 0;
+  algo.rgbMap = function (width, height, rgb, step) {
+      const map = new Array(height);
+      const halfHeight = (height - 1) / 2;
+      const halfWidth = (width - 1) / 2;
+      const halfBarWidth = algo.barWidth / 2;
+      const halfCenterGap = algo.centerGap / 2;
+      const stepCount = algo.rgbMapStepCount(width, height);
+      const preCalculatedAngle = algo.direction * step * (2 * 3.14) / stepCount;
+      
+      for (let y = 0; y < height; y++) {
+          map[y] = new Array(width);
+          for (let x = 0; x < width; x++) {
+              const verticalOffset = halfHeight - y;
+              const horizontalOffset = halfWidth - x;
+              const distance = Math.sqrt(horizontalOffset * horizontalOffset + verticalOffset * verticalOffset);
+              const normalDistance = Math.abs(horizontalOffset * Math.cos(preCalculatedAngle) + verticalOffset * Math.sin(preCalculatedAngle));
+              
+              if (normalDistance <= halfBarWidth && distance >= halfCenterGap)
+                  map[y][x] = rgb;
+              else
+                  map[y][x] = 0;
           }
       }
       return map;
-    };
+  };
 
-    algo.rgbMapStepCount = function(width, height)
-    {
-      return Math.ceil(Math.max(width, height)/4)*8
-    };
+  algo.rgbMapStepCount = function (width, height) {
+      return Math.ceil(Math.max(width, height) / 4) * 8;
+  };
 
-    // Development tool access
-    testAlgo = algo;
+  // Development tool access
+  testAlgo = algo;
 
-    return algo;
-    }
-)();
+  return algo;
+})();
