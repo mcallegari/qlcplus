b67f8934baf6465ccc72681aa359f57489c62f22
Aidan <140276393+skidoodl@users.noreply.github.com>
2023-12-05 14:18:26 -0500

    worked on fill from center... still not finished

--- resources/rgbscripts/straightline.js
+++ resources/rgbscripts/straightline.js
@@ -35,28 +35,32 @@ var testAlgo;
         algo.hlinePixels = 0
         algo.hlineLeft = 0;
         algo.hlineEffect = 0;
+        algo.hlineStretch = 0;
 
         algo.vlineCustom = 0;
         algo.vlinePixels = 0
         algo.vlineUp = 0;
         algo.vlinePlace = 0;
         algo.vlineEffect = 0;
+        algo.vlineStretch = 0;
 
         algo.lineOrientation = 0;
 
         algo.properties.push("name:lineOrientation|type:list|display:Orientation|values:Horizontal (H),Vertical (V)|write:setOrientation|read:getOrientation");
 
         algo.properties.push("name:hlinePlace|type:list|display:H Placement|values:Top,Middle,Bottom,Custom|write:setHPlace|read:getHPlace");
-        algo.properties.push("name:hlineEffect|type:list|display:H Effect|values:None,Fill, Fill from Center,One By One,Noise|write:setHEffect|read:getHEffect");
+        algo.properties.push("name:hlineEffect|type:list|display:H Effect|values:None,Fill,Fill from Center,One By One,Noise|write:setHEffect|read:getHEffect");
         algo.properties.push("name:hlineCustom|type:range|display:H Custom Value|values:0,10000|write:setHCustom|read:getHCustom");
         algo.properties.push("name:hlinePixels|type:range|display:H Remove Pixels|values:0,10000|write:setHPixels|read:getHPixels");
         algo.properties.push("name:hlineLeft|type:range|display:H Move Left|values:0,10000|write:setLeft|read:getLeft");
+        algo.properties.push("name:hlineStretch|type:range|display:H Stretch|values 0,10000|write:setHStretch|read:getHStretch")
 
         algo.properties.push("name:vlinePlace|type:list|display:V Placement|values:Left,Middle,Right,Custom|write:setVPlace|read:getHPlace");
         algo.properties.push("name:vlineEffect|type:list|display:V Effect|values:None,Fill,Fill from Center,One By One,Noise|write:setVEffect|read:getVEffect");
         algo.properties.push("name:vlineCustom|type:range|display:V Custom Value|values:0,10000|write:setVCustom|read:getVCustom");
         algo.properties.push("name:vlinePixels|type:range|display:V Remove Pixels|values:0,10000|write:setVPixels|read:getVPixels");
         algo.properties.push("name:vlineUp|type:range|display:V Move Up|values:0,10000|write:setUp|read:getUp");
+        algo.properties.push("name:vlineStretch|type:range|display:V Stretch|values:0,10000|write:setVStretch|read:getVStretch")
 
         algo.setOrientation = function (_orientation) {
             if (_orientation == "Vertical (V)") {
@@ -138,9 +142,9 @@ var testAlgo;
                 algo.hlineEffect = 1;
             } else if (_effect === "One By One") {
                 algo.hlineEffect = 2;
-            } else if (_effect === "Fill from Center"){
+            } else if (_effect === "Fill from Center") {
                 algo.hlineEffect = 3;
-            } else if (_effect === "Noise"){
+            } else if (_effect === "Noise") {
                 algo.hlineEffect = 4;
             }
         }
@@ -151,9 +155,9 @@ var testAlgo;
                 return "Fill";
             } else if (algo.hlineEffect === 2) {
                 return "One By One";
-            } else if (algo.hlineEffect === 3){
+            } else if (algo.hlineEffect === 3) {
                 return "Fill from Center";
-            } else if (algo.hlineEffect === 4){
+            } else if (algo.hlineEffect === 4) {
                 return "Noise";
             }
         }
@@ -165,9 +169,9 @@ var testAlgo;
                 algo.vlineEffect = 1;
             } else if (_effect === "One By One") {
                 algo.vlineEffect = 2;
-            } else if (_effect === "Fill from Center"){
+            } else if (_effect === "Fill from Center") {
                 algo.vlineEffect = 3;
-            } else if (_effect === "Noise"){
+            } else if (_effect === "Noise") {
                 algo.vlineEffect = 4;
             }
         }
@@ -180,7 +184,7 @@ var testAlgo;
                 return "One By One";
             } else if (algo.vlineEffect === 3) {
                 return "Fill from Center";
-            } else if (algo.vlineEffect === 4){
+            } else if (algo.vlineEffect === 4) {
                 return "Noise";
             }
         }
@@ -221,7 +225,19 @@ var testAlgo;
             return algo.vlineUp;
         };
 
+        algo.setHStretch = function (_stretch) {
+            algo.hlineStretch = _stretch;
+        }; algo.getHStretch = function () {
+            return algo.hlineStretch;
+        }
 
+        algo.setVStretch = function (_stretch) {
+            algo.vlineStretch = _stretch;
+        }; algo.getVStretch = function () {
+            return algo.vlineStretch;
+        }
+
+        var isEven = 0;
         algo.rgbMap = function (width, height, rgb, step) {
             var map = new Array(height);
             if (algo.lineOrientation === 0) { //horizontal
@@ -235,7 +251,7 @@ var testAlgo;
                         effect = (x - algo.hlinePixels <= step);
                     } else if (algo.hlineEffect === 2) { //one by one
                         var xx = step % width;
-                        effect = (x - algo.hlinePixels <= xx) && (x - algo.hlinePixels >= xx);                   
+                        effect = (x - algo.hlinePixels <= xx) && (x - algo.hlinePixels >= xx);
                     }
                     else { //none
                         effect = 1;
@@ -243,7 +259,7 @@ var testAlgo;
 
                     if (algo.hlinePlace === 0) { // top
                         map[0][x - algo.hlineLeft] = (x >= algo.hlinePixels) && (effect) && rgb;
-                    } else if (algo.hlinePlace === 1) { // middles
+                    } else if (algo.hlinePlace === 1) { // middle
                         var middleY = (height / 2)
                         map[middleY][x - algo.hlineLeft] = (x >= algo.hlinePixels) && (effect) && rgb;
                     } else if (algo.hlinePlace === 2) { // bottom
@@ -257,24 +273,20 @@ var testAlgo;
             else if (algo.lineOrientation === 1) { //vertical
                 for (var y = 0; y < height; y++) {
                     map[y] = new Array(width);
-
                     for (var x = 0; x < width; x++) {
                         var effect;
                         var yUp = y - algo.vlineUp;
+                        var center = (Math.floor((parseInt(height) + 1) / 2) - 1);
+                        isEven = (height % 2 === 0);
                         if (algo.vlineEffect === 1) { //fill
                             effect = (y - algo.vlinePixels <= step);
                         } else if (algo.vlineEffect === 2) { //one by one
                             var yy = step % height;
-                        effect = (y - algo.vlinePixels <= yy) && (y - algo.vlinePixels >= yy);
+                            effect = (y - algo.vlinePixels <= yy) && (y - algo.vlinePixels >= yy);
                         } else if (algo.vlineEffect === 3) { //fill from center
-                            isEven =  (width % 2 === 0);
-                            var fill = 0;
-                            if (y <= center + step + (isEven ? 1 : 0 ) && y >= center - step) {
-                                fill = 1;
-                            }
-                            if (fill === 1) {
-                                effect = (y - algo.vlinePixels <= step);   
-                            }
+                            effect = y <= center + step + (isEven ? 1 : 0) && y >= center - step;
+                        } else if (algo.vlineEffect === 4) { //noise
+
                         }
                         else {
                             effect = 1;
@@ -284,8 +296,9 @@ var testAlgo;
                             if (algo.vlinePlace === 0) { // left
                                 map[yUp][0] = (y >= algo.vlinePixels) && effect && rgb;
                             } else if (algo.vlinePlace === 1) { //middle
-                                var middleX = (width / 2)
-                                map[yUp][middleX] = (y >= algo.vlinePixels) && (effect) && rgb;
+                                isEvenW = (width % 2 === 0);
+                                middle = (Math.floor((parseInt(width)+1)/2)-1);
+                                map[yUp][center] = (y >= algo.vlinePixels) && middle + (isEvenW ? 1 : 0) && (effect) && rgb;
                             } else if (algo.vlinePlace === 2) { // Right
                                 map[yUp][width - 1] = (y >= algo.vlinePixels) && (effect) && rgb;
                             } else if (algo.vlinePlace === 3) { // custom
@@ -305,18 +318,18 @@ var testAlgo;
             if (algo.lineOrientation === 0 && algo.hlineEffect === 0) { //horizontal
                 return width;
             }
-            else if (algo.lineOrientation === 0 && algo.hlineEffect === 1 || algo.lineOrientation === 0 && algo.hlineEffect === 2 ) { //fill + one by one
+            else if (algo.lineOrientation === 0 && algo.hlineEffect === 1 || algo.lineOrientation === 0 && algo.hlineEffect === 2) { //fill + one by one
                 return width - algo.hlinePixels;
             }
 
             else if (algo.lineOrientation === 1 && algo.vlineEffect === 0) { //vertical
                 return height;
             }
-            else if (algo.lineOrientation === 1 && algo.vlineEffect === 2 || algo.lineOrientation === 1 && algo.vlineEffect === 1 ) { //fill + one by one
+            else if (algo.lineOrientation === 1 && algo.vlineEffect === 2 || algo.lineOrientation === 1 && algo.vlineEffect === 1) { //fill + one by one
                 return height - algo.vlinePixels;
             }
-            else if (algo.lineOrientation === 1 && algo.vlineEffect === 3){
-                return Math.floor((parseInt(height) + 1) / 2); 
+            else if (algo.lineOrientation === 1 && algo.vlineEffect === 3) { //fill from center
+                return Math.floor((parseInt(height) + 1) / 2) - algo.vlinePixels;
             }
         };
 
