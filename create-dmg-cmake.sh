7b95f958921a2183996c1a747b692111d0448b75
Massimo Callegari <massimocallegari@yahoo.it>
2025-01-19 17:10:52 +0100

    macos: more deployment fixes

--- create-dmg-cmake.sh
+++ create-dmg-cmake.sh
@@ -6,6 +6,10 @@ rm -rf build
 mkdir build
 cd build
 
+if [ -n "$SIGNATURE" ]; then
+    echo "This build WILL NOT be signed. Please export SIGNATURE to sign it."
+fi
+
 # Build
 if [ -n "$QTDIR" ]; then
     cmake -DCMAKE_PREFIX_PATH="$QTDIR/lib/cmake" ..
@@ -41,6 +45,32 @@ platforms/macos/fix_dylib_deps.sh ~/QLC+.app/Contents/Frameworks/libsndfile.1.dy
 echo "Run macdeployqt..."
 $QTDIR/bin/macdeployqt ~/QLC+.app
 
+if [ -n "$SIGNATURE" ]; then
+    # sign package with codesign (macdeployqt fails in that too)
+    echo "Signing binaries..."
+    find ~/QLC+.app/Contents/Frameworks -type f | while read file; do
+        codesign --force --sign "$SIGNATURE" --timestamp "$file"
+    done
+
+    find ~/QLC+.app/Contents/PlugIns -type f -name "*.dylib" | while read file; do
+        codesign --force --sign "$SIGNATURE" --timestamp "$file"
+    done
+
+    find ~/QLC+.app/Contents/MacOS -type f | while read file; do
+        codesign --force --sign "$SIGNATURE" --timestamp "$file"
+    done
+
+    codesign --sign "$SIGNATURE" \
+            --entitlements platforms/macos/qlcplus.entitlements \
+            --timestamp \
+            --deep \
+            --options runtime \
+            ~/QLC+.app
+
+    # workaround first time sign failure 
+    codesign --force --sign "$SIGNATURE" --timestamp  ~/QLC+.app/Contents/MacOS/qlcplus-launcher
+fi
+
 #echo "Fix some more dependencies..."
 #install_name_tool -change /usr/local/opt/fftw/lib/libfftw3.3.dylib @executable_path/../Frameworks/libfftw3.3.dylib ~/QLC+.app/Contents/MacOS/qlcplus
 #install_name_tool -change /usr/local/opt/fftw/lib/libfftw3.3.dylib @executable_path/../Frameworks/libfftw3.3.dylib ~/QLC+.app/Contents/MacOS/qlcplus-fixtureeditor 
@@ -59,3 +89,10 @@ cd platforms/macos/dmg
     $OUTDIR/QLC+_$VERSION.dmg \
     ~/QLC+.app
 cd -
+
+if [ -n "$SIGNATURE" ]; then
+    codesign --sign "$SIGNATURE" \
+             --entitlements platforms/macos/qlcplus.entitlements \
+             --timestamp \
+             $OUTDIR/QLC+_$VERSION.dmg
+fi
