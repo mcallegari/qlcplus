6d01480837c6d8e7ef64fca89480b0fb65a4f848
Massimo Callegari <massimocallegari@yahoo.it>
2025-03-09 19:20:46 +0100

    macos: improve deployment

--- create-dmg-cmake.sh
+++ create-dmg-cmake.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #VERSION=$(head -1 debian/changelog | sed 's/.*(\(.*\)).*/\1/')
 VERSION=$(grep -m 1 APPVERSION variables.pri | cut -d '=' -f 2 | sed -e 's/^[[:space:]]*//' | tr ' ' _ | tr -d '\r\n')
+APP_DIR=~/QLC+.app
+
+# cleanup previous builds
+rm -rf $APP_DIR
+rm *.dmg
+rm rw.*
 
 rm -rf build
 mkdir build
@@ -40,34 +46,35 @@ fi
 cd ..
 
 echo "Fix non-Qt dependencies..."
-platforms/macos/fix_dylib_deps.sh ~/QLC+.app/Contents/Frameworks/libsndfile.1.dylib
-platforms/macos/fix_dylib_deps.sh ~/QLC+.app/Contents/MacOS/qlcplus
-platforms/macos/fix_dylib_deps.sh ~/QLC+.app/Contents/MacOS/qlcplus-fixtureeditor
+platforms/macos/fix_dylib_deps.sh $APP_DIR/Contents/Frameworks/libsndfile.1.dylib
+platforms/macos/fix_dylib_deps.sh $APP_DIR/Contents/MacOS/qlcplus
+platforms/macos/fix_dylib_deps.sh $APP_DIR/Contents/MacOS/qlcplus-fixtureeditor
 
 echo "Run macdeployqt..."
-$QTDIR/bin/macdeployqt ~/QLC+.app
+$QTDIR/bin/macdeployqt $APP_DIR
 
 if [ -n "$SIGNATURE" ]; then
     # sign package with codesign (macdeployqt fails in that too)
     echo "Signing binaries..."
     ENTITLEMENTS="platforms/macos/qlcplus.entitlements"
 
-    find ~/QLC+.app/Contents/Frameworks -type f | while read file; do
+    find $APP_DIR/Contents/Frameworks -type f | while read file; do
         codesign --force --sign "$SIGNATURE" --timestamp "$file"
     done
 
-    find ~/QLC+.app/Contents/PlugIns -type f -name "*.dylib" | while read file; do
+    find $APP_DIR/Contents/PlugIns -type f -name "*.dylib" | while read file; do
         codesign --force --sign "$SIGNATURE" --timestamp "$file"
     done
 
-    find ~/QLC+.app/Contents/MacOS -type f | while read file; do
+    find $APP_DIR/Contents/MacOS -type f | while read file; do
         codesign --force --sign "$SIGNATURE" --timestamp --entitlements $ENTITLEMENTS --options runtime "$file"
     done
 
-    codesign --sign "$SIGNATURE" --timestamp --deep --options runtime ~/QLC+.app
+    codesign --sign "$SIGNATURE" --timestamp --deep --entitlements $ENTITLEMENTS --options runtime $APP_DIR
 
-    # workaround first time sign failure 
-    codesign --force --sign "$SIGNATURE" --timestamp --options runtime ~/QLC+.app/Contents/MacOS/qlcplus-launcher
+    # workaround first time sign failure
+    codesign --force --sign "$SIGNATURE" --timestamp --entitlements $ENTITLEMENTS --options runtime $APP_DIR/Contents/MacOS/qlcplus
+    codesign --force --sign "$SIGNATURE" --timestamp --entitlements $ENTITLEMENTS --options runtime $APP_DIR/Contents/MacOS/qlcplus-launcher
 fi
 
 # Create Apple Disk iMaGe from ~/QLC+.app/
@@ -82,7 +89,7 @@ cd platforms/macos/dmg
     --icon "QLC+" 0 150 \
     --app-drop-link 200 150 \
     $OUTDIR/QLC+_$VERSION.dmg \
-    ~/QLC+.app
+    $APP_DIR
 cd -
 
 if [ -n "$SIGNATURE" ]; then
