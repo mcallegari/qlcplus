77615ce5d9428570a6c5f7bb5afbdc8a6d224e5a
Massimo Callegari <massimocallegari@yahoo.it>
2025-11-24 21:09:04 +0100

    engine: give proper credit for beat tracking

--- engine/audio/src/beattracking.h
+++ engine/audio/src/beattracking.h
@@ -2,7 +2,7 @@
   Q Light Controller Plus
   beattracking.h
 
-  Copyright (c) Massimo Callegari
+  Copyright (c) Dennis Suermann
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
