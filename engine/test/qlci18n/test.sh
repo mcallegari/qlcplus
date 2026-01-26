040d8d167a7368c8ad9f241170fe7d84db802bea
jpue <94829332+jpue@users.noreply.github.com>
2026-01-26 01:09:05 +0100

    engine/qlci18n_test: finish removing qmake

--- engine/test/qlci18n/test.sh
+++ engine/test/qlci18n/test.sh
@@ -3,10 +3,10 @@ export LD_LIBRARY_PATH=../../src
 export DYLD_FALLBACK_LIBRARY_PATH=../../src
 cp qlci18n_fi_FI.ts_ qlci18n_fi_FI.ts
 if [ -n "$QTDIR" ]; then
-    $QTDIR/bin/lupdate qlci18n.pro
-    $QTDIR/bin/lrelease qlci18n.pro
+    $QTDIR/bin/lupdate . -ts qlci18n_fi_FI.ts
+    $QTDIR/bin/lrelease -silent qlci18n_fi_FI.ts -qm qlci18n_fi_FI.qm
 else
-    lupdate qlci18n.pro
-    lrelease qlci18n.pro
+    lupdate . -ts qlci18n_fi_FI.ts
+    lrelease -silent qlci18n_fi_FI.ts -qm qlci18n_fi_FI.qm
 fi
 ./qlci18n_test
